6ca485096b0e56084e949eaf07ea44013408bdee
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2023-04-25 14:00:18 +0200

    Update portugals.sh

--- feed/portugals.sh
+++ feed/portugals.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Thanks to LITUATUI user on github
 
 sed -i "/live_cnn/ c  https://video-auth7.iol.pt/live_cnn/live_cnn/playlist.m3u8?wmsAuthSign=$(wget https://services.iol.pt/matrix?userId= -o /dev/null -O -)" all
 
