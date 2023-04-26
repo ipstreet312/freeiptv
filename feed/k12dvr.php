bbfb63caff089d46a48eb6112eb62375e1222215
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2023-04-27 01:41:30 +0200

    Update k12dvr.php

--- feed/k12dvr.php
+++ feed/k12dvr.php
@@ -1,5 +1,5 @@
 <?php
-$url = 'https://mass.mako.co.il/ClicksStatistics/entitlementsServicesV2.jsp?et=ngt&lp=/direct/hls/live/2033791/k12dvr/index.m3u8?as=1&rv=AKAMAI';
+$url = 'https://mass.mako.co.il/ClicksStatistics/entitlementsServicesV2.jsp?et=ngt&lp=/direct/hls/live/2033791/k12dvr/index.m3u8?b-in-range=800-2700&rv=AKAMAI';
 
 $content = file_get_contents($url);
 $json = json_decode($content, true);
