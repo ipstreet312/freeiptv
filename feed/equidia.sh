f123c965ca8c41bd95d44d8dd872149ba08b5193
schumijo <40740060+schumijo@users.noreply.github.com>
2023-07-26 07:20:54 +0200

    Update equidia.sh

--- feed/equidia.sh
+++ feed/equidia.sh
@@ -2,6 +2,6 @@
 # Thanks to pshanmu3 user on github
 echo $(dirname $0)
 cd $(dirname $0)
-python3 equidia.py http://62.210.135.99:80 > ../feed/equidia.m3u8
+python3 equidia.py http://62.210.135.99:80 > equidia.m3u8
 
 echo m3u grabbed
