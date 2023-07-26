7aa6fa5e9220624740edde53b24fccfb057565fb
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2023-07-26 15:56:01 +0200

    Update frinfo.sh

--- ressources/dmotion/py/frinfo/frinfo.sh
+++ ressources/dmotion/py/frinfo/frinfo.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-current_directory="$PWD"
-
-echo "Current working directory: $current_directory"
-
-#python3 /home/runner/work/freeiptv/freeiptv/ressources/dmotion/py/frinfo/frinfo.py > frinfo.m3u8
+python3 frinfo.py > frinfo.m3u8
 
 exit 0
 
