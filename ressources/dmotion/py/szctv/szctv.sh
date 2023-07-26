dabf163c277d75309d95e866f554fbe696802ca4
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2023-07-26 16:32:45 +0200

    Update szctv.sh

--- ressources/dmotion/py/szctv/szctv.sh
+++ ressources/dmotion/py/szctv/szctv.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-# Thanks to pshanmu3 user on github
-echo $(dirname $0)
-cd $(dirname $0)
-python3 szctv.py > ../ressources/dmotion/py/szctv/szctv.m3u8
 
-echo m3u grabbed
+python3 ressources/dmotion/py/szctv/szctv.py > ressources/dmotion/py/szctv/szctv.m3u8
+
+exit 0
