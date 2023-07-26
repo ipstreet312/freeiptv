778a7849c0f00c6504601412093ed0a4f83eaff3
schumijo <40740060+schumijo@users.noreply.github.com>
2023-07-26 07:09:17 +0200

    Add proxy and var

--- feed/equidia.sh
+++ feed/equidia.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-cd /home/runner/work/freeiptv/feed
-python3 equidia.py > ./feed/equidia.m3u
+echo $(dirname $0)
+cd $(dirname $0)
+python3 equidia.py http://62.210.135.99:80 > ../feed/equidia.m3u8
 
 echo m3u grabbed
