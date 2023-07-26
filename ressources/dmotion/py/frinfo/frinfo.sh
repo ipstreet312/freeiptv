24411741cad83d527601b0ab2c8d0a41adf89ec8
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2023-07-26 15:15:27 +0200

    Update frinfo.sh

--- ressources/dmotion/py/frinfo/frinfo.sh
+++ ressources/dmotion/py/frinfo/frinfo.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 cd ressources/dmotion/py/frinfo
-python3 frinfo.py > frinfo.m3u8
 
-echo m3u grabbed
+python frinfo.py > frinfo.m3u8
 
-# exit 0
+exit 0
+
+#cd ressources/dmotion/py/frinfo
+#python3 frinfo.py > frinfo.m3u8
+#echo m3u grabbed
+#exit 0
