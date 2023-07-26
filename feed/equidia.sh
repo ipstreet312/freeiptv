3a5583d080b3a4a39077164ba5cdf34a8e413a49
schumijo <40740060+schumijo@users.noreply.github.com>
2023-07-26 07:13:54 +0200

    Update equidia.sh

--- feed/equidia.sh
+++ feed/equidia.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-echo $(dirname $0)
-cd $(dirname $0)
-python3 equidia.py http://62.210.135.99:80 > ../feed/equidia.m3u8
+cd /home/runner/work/freeiptv/feed
+python3 equidia.py http://62.210.135.99:80 > ./feed/equidia.m3u8
 
 echo m3u grabbed
