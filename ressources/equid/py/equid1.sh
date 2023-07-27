7724c2efa40dfd55d210ca75d12d22cb9fd7afcd
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2023-07-27 10:37:07 +0200

    Create equid1.sh

---
+++ ressources/equid/py/equid1.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sed -i "/equidia-website-web-1/ c  $(wget https://raw.githubusercontent.com/ipstreet312/freeiptv/master/ressources/equid/py/equid1.m3u8 -o /dev/null -O -)" all.m3u
+
+exit 0
