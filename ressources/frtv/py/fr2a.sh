4028b1c373bcb9f2458a5934a77ac508bcbf63d5
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2023-07-27 19:52:59 +0200

    Delete ressources/frtv/py directory

--- ressources/frtv/py/fr2a.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python3 ressources/frtv/py/fr2a.py > ressources/frtv/py/fr2a.m3u8
-
-exit 0
--- ressources/frtv/py/fr2b.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python3 ressources/frtv/py/fr2b.py > ressources/frtv/py/fr2b.m3u8
-
-exit 0
