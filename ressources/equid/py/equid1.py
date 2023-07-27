1e2e1a0cb27f142f87ab09d56130eb8da1013709
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2023-07-27 10:35:38 +0200

    Create equid1.py

---
+++ ressources/equid/py/equid1.py
@@ -0,0 +1,10 @@
+#! /usr/bin/python3
+
+import requests
+import json
+import time
+
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+response = requests.get('https://equidia-live2-p-api.hexaglobe.net/0d62898eeb26305306d2af3e6ec3d54d/64b8fb4b/equidia/equidia-website-web-1/live2/stream_info.php').json()[0]['primary']
+m3u = requests.get(response).text
+print(m3u)
