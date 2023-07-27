0de68f830d16279373c392bedb819a03639ddee7
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2023-07-27 11:11:35 +0200

    Update equid1.py

--- ressources/equid/py/equid1.py
+++ ressources/equid/py/equid1.py
@@ -5,6 +5,7 @@ import json
 import time
 
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-response = requests.get('https://equidia-live2-p-api.hexaglobe.net/0d62898eeb26305306d2af3e6ec3d54d/64b8fb4b/equidia/equidia-website-web-1/live2/stream_info.php').json()['primary']
-m3u = requests.get(response).text
-print(m3u)
+response = requests.get('https://equidia-live2-p-api.hexaglobe.net/0d62898eeb26305306d2af3e6ec3d54d/64b8fb4b/equidia/equidia-website-web-1/live2/stream_info.php')
+response_json = json.loads(response.text)
+m3u8 = response_json["primary"]
+print(m3u8)
