d05bce1b1fc1cd1c545d31f32c2443da80d025eb
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2023-07-27 11:41:11 +0200

    Update equid1.py

--- ressources/equid/py/equid1.py
+++ ressources/equid/py/equid1.py
@@ -8,7 +8,7 @@ headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleW
 
 session = requests.Session()
 response = session.get('https://equidia-live2-p-api.hexaglobe.net/0d62898eeb26305306d2af3e6ec3d54d/64b8fb4b/equidia/equidia-website-web-1/live2/stream_info.php')
-response_json = json.loads(response.text)
+response_json = json.loads(response)
 g1 = response_json["primary"]
 res2 = requests.get(g1)
 data = res2.text
