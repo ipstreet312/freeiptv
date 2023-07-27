b7d1639d422c95f12fb198cb22cec1caf3b05403
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2023-07-27 19:34:52 +0200

    Create fr2b.py

---
+++ ressources/frtv/py/fr2b.py
@@ -0,0 +1,16 @@
+#! /usr/bin/python3
+
+import requests
+import json
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'
+}
+
+session = requests.Session()
+response = session.get('https://hdfauth.ftven.fr/esi/TA?format=json&url=https://simulcast-p.ftven.fr/simulcast/France_2/hls_fr2/index.m3u8')
+
+response_json = json.loads(response.text)
+g1 = response_json["url"]
+res2 = requests.get(g1)
+print(res2)
