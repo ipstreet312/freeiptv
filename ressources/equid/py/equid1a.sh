9c9a51d1d7cd0a9165c8e8bee1402d92640e962d
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2023-07-28 17:21:41 +0200

    Delete ressources/equid/py directory

--- ressources/equid/py/equid1a.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python3 ressources/equid/py/equid1.py > ressources/equid/py/equid1.m3u8
-
-exit 0
--- ressources/equid/py/equid1b.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sed -i "/lci-cmaf/ c  $(wget https://raw.githubusercontent.com/ipstreet312/freeiptv/master/ressources/equid/py/equid1.m3u8 -o /dev/null -O -)" all.m3u
-
-exit 0
