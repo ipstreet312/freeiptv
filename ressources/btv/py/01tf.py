448e68023aa37aa76d9d05b9193c849efc2d2287
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2023-07-31 21:53:58 +0200

    Update 01tf.py

--- ressources/btv/py/01tf.py
+++ ressources/btv/py/01tf.py
@@ -4,6 +4,7 @@ import requests
 import os
 import sys
 import time
+import re
 
 headers={
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.0",
@@ -12,4 +13,28 @@ headers={
 
 s = requests.Session()
 response = s.get('http://www.callofliberty.fr/stream/TF1/master.m3u8', headers=headers)
-print(response.text)
+
+#print(response.text)
+
+def extract_desired_part(url):
+    pattern = r'tf1\.fr/(.*?)/out/v1/780542f516f143fc8ad9bf14622316e7/tf1-cmaf/index_4'
+
+    match = re.search(pattern, url)
+    
+    if match:
+        extracted_part = match.group(1)
+    else:
+        extracted_part = None
+    
+    return extracted_part
+
+links = re.findall(r'https?://\S+', response)
+
+desired_parts = []
+
+for link in links:
+    extracted_part = extract_desired_part(link)
+    if extracted_part:
+        desired_parts.append(extracted_part)
+
+print(desired_parts.text)
