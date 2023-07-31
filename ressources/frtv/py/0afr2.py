7deb36110cb60ae509a869ab41d8769aa9eefbf9
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2023-07-31 19:33:19 +0200

    Create 0afr2.py

---
+++ ressources/frtv/py/0afr2.py
@@ -0,0 +1,8 @@
+#! /usr/bin/python3
+
+import requests
+
+session = requests.Session()
+response = session.get('https://hdfauth.ftven.fr/esi/TA?url=https://simulcast-p.ftven.fr/simulcast/France_2/hls_fr2/index.m3u8')
+
+print(response)
