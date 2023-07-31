a90f7b29e7242e219aebce877884b16b27f2576e
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2023-07-31 19:37:29 +0200

    Delete 0bfr2.py

--- ressources/frtv/py/0bfr2.py
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/python3
-
-import requests
-
-session = requests.Session()
-response = session.get('https://raw.githubusercontent.com/ipstreet312/freeiptv/master/ressources/frtv/py/0fr2.m3u8')
-
-print(response)
