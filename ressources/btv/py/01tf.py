600b5d059570b3067d6b84ecedf16bcda7833c48
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2023-07-31 17:58:22 +0200

    Create 01tf.py

---
+++ ressources/btv/py/01tf.py
@@ -0,0 +1,14 @@
+#! /usr/bin/python3
+
+import requests
+import os
+import sys
+import time
+
+headers={
+        "Referer": "http://www.callofliberty.fr/tv/1-TF1/1-TF1.php/"
+}
+
+s = requests.Session()
+response = s.get('http://www.callofliberty.fr/stream/TF1/master.m3u8', headers=headers)
+print(response)
