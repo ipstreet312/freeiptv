fdcf345c7a75a8b03239fa6c06b14beb70363f44
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2023-07-31 20:23:40 +0200

    Update 0afr2.py

--- ressources/frtv/py/0afr2.py
+++ ressources/frtv/py/0afr2.py
@@ -9,8 +9,7 @@ print('#EXT-X-VERSION:3')
 print('#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=2560000')
 s = requests.Session()
 response = s.get(f'https://hdfauth.ftven.fr/esi/TA?url=https://simulcast-p.ftven.fr/simulcast/France_2/hls_fr2/index.m3u8')
-m3u = s.get(response)
-print(m3u)
+print(response)
             
 if 'temp.txt' in os.listdir():
     os.system('rm temp.txt')
