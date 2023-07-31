af9b833d33eb502431f1269b82a2b10cbf8f0524
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2023-07-31 21:04:23 +0200

    Update 01tf.py

--- ressources/btv/py/01tf.py
+++ ressources/btv/py/01tf.py
@@ -6,8 +6,8 @@ import sys
 import time
 
 headers={
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.0",
         "Referer": "http://www.callofliberty.fr/"
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.0"
 }
 
 s = requests.Session()
