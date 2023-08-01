257bdb60cfaf73b83970c0ebe049efa5ccf0cd90
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2023-08-01 05:24:06 +0200

    Update hglobal.sh

--- ressources/dmotion/py/hglobal/hglobal.sh
+++ ressources/dmotion/py/hglobal/hglobal.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-# Thanks to LITUATUI user on github
-cd ressources/dmotion/py/hglobal
 
-python hglobal.py && sed -e '/x81x3cd/ {' -e 'r hglobal.txt' -e 'd' -e '}' -i hglobal.m3u8
+python3 ressources/dmotion/py/fbtv/hglobal.py > ressources/dmotion/py/fbtv/hglobal.m3u8
 
 exit 0
