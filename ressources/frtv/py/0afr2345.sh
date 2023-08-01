bb98fc592db83037e49ea89d6fc7731c17b41d21
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2023-08-01 06:51:56 +0200

    Create 0afr2345.sh

---
+++ ressources/frtv/py/0afr2345.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+python3 ressources/frtv/py/0afr2.py > ressources/frtv/py/0fr2.m3u8
+python3 ressources/frtv/py/0afr3.py > ressources/frtv/py/0fr3.m3u8
+python3 ressources/frtv/py/0afr4.py > ressources/frtv/py/0fr4.m3u8
+python3 ressources/frtv/py/0afr5.py > ressources/frtv/py/0fr5.m3u8
+
+exit 0
