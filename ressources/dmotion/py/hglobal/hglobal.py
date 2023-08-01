f9e01d6d452d5221afb68a3c43014c2a861ca8d0
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2023-08-01 05:23:12 +0200

    Update hglobal.py

--- ressources/dmotion/py/hglobal/hglobal.py
+++ ressources/dmotion/py/hglobal/hglobal.py
@@ -1,37 +1,58 @@
-#!/usr/bin/env python
+#! /usr/bin/python3
+# Thanks to pshanmu3 user on github
+import requests
+import os
+import sys
 
-# Thanks to LITUATUI user on github
+proxies = {}
+if len(sys.argv) == 2:
+    proxies = {
+                'http' : sys.argv[1],
+                'https' : sys.argv[1]
+              }
 
-import os, requests, json, time, sys
-os.system("clear")
+na = 'https://raw.githubusercontent.com/naveenland4/UTLive/main/assets/info.m3u8'
+def grab(line):
+    try:
+        _id = line.split('/')[4]
+        response = s.get(f'https://www.dailymotion.com/player/metadata/video/{_id}', proxies=proxies).json()['qualities']['auto'][0]['url']
+        m3u = s.get(response, proxies=proxies).text
+        m3u = m3u.strip().split('\n')[1:]
+        d = {}
+        cnd = True
+        for item in m3u:
+            if cnd:
+                resolution = item.strip().split(',')[2].split('=')[1]
+                if resolution not in d:
+                    d[resolution] = []
+            else:
+                d[resolution]= item
+            cnd = not cnd
+        #print(m3u)
+        m3u = d[max(d, key=int)]    
+    except Exception as e:
+        m3u = na
+    finally:
+        print(m3u)
 
-url = "https://www.dailymotion.com/video/x81x3cd"
-
-def download_data(url):
-    p1 = "https://api.w03.savethevideo.com/tasks"
-
-    obj = {"type":"info","url": url }
-
-    r = requests.post(p1, json = obj)
-    h = json.loads(r.text)['href']
-
-    print(h)
-
-    g1 = "https://api.w03.savethevideo.com//" + h
-    time.sleep(3)
-    r2 = requests.get(g1)
-
-    jr2 = json.loads(r2.text)
-
-    #print(jr2)
-    print(jr2['result']['title'])
-    sys.stdout=open("hglobal.txt","w")
-    print(jr2['result']['url'])
-    sys.stdout.close()
-
-    return_data = {
-        "title" : jr2['result']['title'],
-        "url" : jr2['result']['url']    
-    }
-
-download_data(url)
+print('#EXTM3U')
+print('#EXT-X-VERSION:3')
+print('#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=2560000')
+s = requests.Session()
+with open('ressources/dmotion/py/fbtv/hglobal_info.txt') as f:
+    for line in f:
+        line = line.strip()
+        if not line or line.startswith('~~'):
+            continue
+        if not line.startswith('https:'):
+            line = line.split('|')
+            ch_name = line[0].strip()
+            grp_title = line[1].strip().title()
+            tvg_logo = line[2].strip()
+            tvg_id = line[3].strip()
+        else:
+            grab(line)
+            
+if 'temp.txt' in os.listdir():
+    os.system('rm temp.txt')
+    os.system('rm watch*')
