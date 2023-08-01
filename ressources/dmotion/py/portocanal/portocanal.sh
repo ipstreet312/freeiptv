435abbf6d91bfd8400b330baf9a5f85736b72d01
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2023-08-01 05:31:17 +0200

    Delete ressources/dmotion/py/portocanal directory

--- ressources/dmotion/py/portocanal/portocanal.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Thanks to LITUATUI user on github
-cd ressources/dmotion/py/portocanal
-
-python portocanal.py && sed -e '/x8egnb8/ {' -e 'r portocanal.txt' -e 'd' -e '}' -i portocanal.m3u8
-
-exit 0
