bd77e42f2f2d0556ebf9a5bd713f9d5ae9cd75dd
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2023-08-01 06:37:23 +0200

    Update 0afr345.sh

--- ressources/frtv/py/0afr345.sh
+++ ressources/frtv/py/0afr345.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-sed -i "/France_3/hls_fr3/ c  $(wget https://raw.githubusercontent.com/ipstreet312/freeiptv/master/ressources/frtv/py/0fr3.m3u8 -o /dev/null -O -)" all.m3u
+sed -i '/France_3/hls_fr3/ c '"$(wget -qO- https://raw.githubusercontent.com/ipstreet312/freeiptv/master/ressources/frtv/py/0fr3.m3u8)" all.m3u
 
-sed -i "/France_4/hls_fr4/ c  $(wget https://raw.githubusercontent.com/ipstreet312/freeiptv/master/ressources/frtv/py/0fr4.m3u8 -o /dev/null -O -)" all.m3u
+sed -i '/France_4/hls_fr4/ c '"$(wget -qO- https://raw.githubusercontent.com/ipstreet312/freeiptv/master/ressources/frtv/py/0fr4.m3u8)" all.m3u
 
-sed -i "/France_5/hls_fr5/ c  $(wget https://raw.githubusercontent.com/ipstreet312/freeiptv/master/ressources/frtv/py/0fr5.m3u8 -o /dev/null -O -)" all.m3u
+sed -i '/France_5/hls_fr5/ c '"$(wget -qO- https://raw.githubusercontent.com/ipstreet312/freeiptv/master/ressources/frtv/py/0fr5.m3u8)" all.m3u
 
 exit 0
