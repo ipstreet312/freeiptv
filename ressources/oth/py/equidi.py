9aaf1f3da186aaa5f07efc9002229c8d8138af91
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2023-08-04 02:31:29 +0200

    Create equidi.py

---
+++ ressources/oth/py/equidi.py
@@ -0,0 +1,21 @@
+#! /usr/bin/python3
+
+import requests
+import os
+import sys
+import json
+import time
+
+
+headers={
+        "User-Agent": "Equidia/6036 CFNetwork/1220.1 Darwin/20.3.0",
+        "Referer": "https://fr.equidia.app/"
+}
+
+s = requests.Session()
+response = s.get('https://api.equidia.fr/api/public/racing/equidia-mobileapp-ios-1/equidia-live2', headers=headers).json()['primary']
+#print(response)
+print('#EXTM3U')
+print('#EXT-X-VERSION:3')
+print('#EXT-X-STREAM-INF:BANDWIDTH=2270400,RESOLUTION=1280x720')
+print(response.replace("playlist.m3u8", "eqd" + id + "_fre_1.m3u8"))
