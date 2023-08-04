edab99c9d5cf53d7749a2a3db7bb01dacf1abb56
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2023-08-04 02:57:51 +0200

    Delete ressources/froth/py directory

--- ressources/froth/py/equidv1b.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sed -i "/eqdlivepri_fre.smil/ c  $(wget https://raw.githubusercontent.com/ipstreet312/freeiptv/master/ressources/froth/py/equidv1.m3u8 -o /dev/null -O -)" all.m3u
-
-exit 0
