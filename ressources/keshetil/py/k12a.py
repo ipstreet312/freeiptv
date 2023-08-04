a854c8438cc1ca665f37ef1fe626c022b15cae43
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2023-08-04 13:06:48 +0200

    Create k12a.py

---
+++ ressources/keshetil/py/k12a.py
@@ -0,0 +1,14 @@
+#! /usr/bin/python3
+
+import requests
+import os
+import sys
+import json
+
+s = requests.Session()
+toki = s.get('https://mass.mako.co.il/ClicksStatistics/entitlementsServicesV2.jsp?et=ngt&lp=/direct/hls/live/2033791/k12/index.m3u8?as=1&rv=AKAMAI').json()['tickets'][0]['ticket']
+master = 'https://mako-streaming.akamaized.net/direct/hls/live/2033791/k12/index.m3u8'
+final_master = f'{master}?{toki}'
+#print(final_master)
+chunks = f'{final_master}'
+print(chunks)
