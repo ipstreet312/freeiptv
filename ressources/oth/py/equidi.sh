35c48d353093b27db006a666ce43315fc640e2bc
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2023-08-14 09:44:25 +0200

    Delete ressources/oth/py directory

--- ressources/oth/py/equidi.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python3 ressources/oth/py/equidi.py > ressources/oth/py/equidi.m3u8
-
-exit 0
--- ressources/oth/py/espsor1.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python3 ressources/oth/py/espsor1.py > ressources/oth/py/espsor1.m3u8
-
-exit 0
