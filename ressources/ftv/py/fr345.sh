60fd2ec4645aaefc66df0cd79bada46b69e74549
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2023-08-21 20:54:43 +0200

    Update fr345.sh

--- ressources/ftv/py/fr345.sh
+++ ressources/ftv/py/fr345.sh
@@ -2,6 +2,10 @@
 
 content=$(<ressources/ftv/py/fr2.m3u8)
 
-fr3_content="${content/France_2/France_3}"
-fr3_content="${fr3_content/hls_fr2/hls_fr3}"
+fr3_content=$(echo "$content" | sed 's/France_2/France_3/g; s/hls_fr2/hls_fr3/g')
+fr4_content=$(echo "$content" | sed 's/France_2/France_4/g; s/hls_fr2/hls_fr4/g')
+fr5_content=$(echo "$content" | sed 's/France_2/France_5/g; s/hls_fr2/hls_fr5/g')
+
 echo "$fr3_content" > ressources/ftv/py/fr3.m3u8
+echo "$fr4_content" > ressources/ftv/py/fr4.m3u8
+echo "$fr5_content" > ressources/ftv/py/fr5.m3u8
