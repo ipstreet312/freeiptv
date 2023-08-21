f5d9b3ce58b8d5bc704d7f2e163e408cc77b5408
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2023-08-21 21:00:23 +0200

    Update fr345.sh

--- ressources/ftv/py/fr345.sh
+++ ressources/ftv/py/fr345.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
-content=$(<ressources/ftv/py/fr2.m3u8)
+input_file="ressources/ftv/py/fr2.m3u8"
+output_dir="ressources/ftv/py"
 
-fr3_content=$(echo "$content" | sed 's/France_2/France_3/g; s/hls_fr2/hls_fr3/g')
-fr4_content=$(echo "$content" | sed 's/France_2/France_4/g; s/hls_fr2/hls_fr4/g')
-fr5_content=$(echo "$content" | sed 's/France_2/France_5/g; s/hls_fr2/hls_fr5/g')
+if [ ! -f "$input_file" ]; then
+    echo "Input file $input_file not found!"
+    exit 1
+fi
 
-echo "$fr3_content" > ressources/ftv/py/fr3.m3u8
-echo "$fr4_content" > ressources/ftv/py/fr4.m3u8
-echo "$fr5_content" > ressources/ftv/py/fr5.m3u8
+if [ ! -d "$output_dir" ]; then
+    echo "Output directory $output_dir not found!"
+    exit 1
+fi
+
+fr3_content=$(cat "$input_file" | sed 's/France_2/France_3/g; s/hls_fr2/hls_fr3/g')
+fr4_content=$(cat "$input_file" | sed 's/France_2/France_4/g; s/hls_fr2/hls_fr4/g')
+fr5_content=$(cat "$input_file" | sed 's/France_2/France_5/g; s/hls_fr2/hls_fr5/g')
+
+echo "$fr3_content" > "$output_dir/fr3.m3u8"
+echo "$fr4_content" > "$output_dir/fr4.m3u8"
+echo "$fr5_content" > "$output_dir/fr5.m3u8"
