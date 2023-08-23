7a0e35b579a02d9f600b606d1ac93a75ca4a20a5
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2023-08-23 09:30:51 +0200

    Update rmcst.sh

--- ressources/dmotion/py/rmcst/rmcst.sh
+++ ressources/dmotion/py/rmcst/rmcst.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python3 ressources/dmotion/py/rmcst/rmcst.py > ressources/dmotion/py/rmcst/rmcst.m3u8
+python3 ressources/dmotion/py/rmcst/rmcst.py http://62.210.135.99:80 > ressources/dmotion/py/rmcst/rmcst.m3u8
 
 exit 0
