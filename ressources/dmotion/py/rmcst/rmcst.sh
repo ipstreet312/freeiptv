cd0c585758bb8d2a1b1b8508652ccabb43536aba
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2023-08-23 09:38:37 +0200

    Update rmcst.sh

--- ressources/dmotion/py/rmcst/rmcst.sh
+++ ressources/dmotion/py/rmcst/rmcst.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python3 ressources/dmotion/py/rmcst/rmcst.py > ressources/dmotion/py/rmcst/rmcst.m3u8
+python3 ressources/dmotion/py/rmcst/rmcst.py http://lhgeo.fr/p > ressources/dmotion/py/rmcst/rmcst.m3u8
 
 exit 0
