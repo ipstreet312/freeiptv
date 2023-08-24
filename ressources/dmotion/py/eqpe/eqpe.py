a0e0b853a1df5c77aeca9f8f8b34c62cbf9d6fe6
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2023-08-24 07:00:20 +0200

    Update eqpe.py

--- ressources/dmotion/py/eqpe/eqpe.py
+++ ressources/dmotion/py/eqpe/eqpe.py
@@ -1,31 +1,34 @@
 #!/usr/bin/python3
 
 import requests
-import os
-import sys
 
-na = 'https://empty'
 print('#EXTM3U')
 print('#EXT-X-VERSION:3')
-print('#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=2560000')
+print('#EXT-X-STREAM-INF:RESOLUTION=1280x720,FRAME-RATE=25.000000,BANDWIDTH=2179072,CODECS="avc1.64001f,mp4a.40.2",NAME="720"')
 
+url = 'https://www.dailymotion.com/player/metadata/video/x8i9j7s'
 try:
-    s = requests.Session()
-    response = s.get(f'https://www.dailymotion.com/player/metadata/video/x8i9j7s').json()['qualities']['auto'][0]['url']
-    m3u = s.get(response).text
-    m3u = m3u.strip().split('\n')[1:]
-    d = {}
-    cnd = True
-    for item in m3u:
-        if cnd:
-            resolution = item.strip().split(',')[2].split('=')[1]
-            if resolution not in d:
-                d[resolution] = []
+    response = requests.get(url)
+    response.raise_for_status()  # Raise an exception for HTTP errors
+    video_data = response.json()
+    master_url = video_data['qualities']['auto'][0]['url']
+    
+    def get_specific_line_online(url, line_number):
+        response = requests.get(url)
+        if response.status_code == 200:
+            lines = response.text.split('\n')
+            if 1 <= line_number <= len(lines):
+                return lines[line_number - 1]
             else:
-                d[resolution] = item
-            cnd = not cnd
-    m3u = d[max(d, key=int)]
-except Exception as e:
-    m3u = na
-finally:
-    print(m3u)
+                return None
+        else:
+            return None
+    
+    chunks = get_specific_line_online(master_url, 15)
+    print(chunks)
+except requests.exceptions.RequestException as e:
+    print("An error occurred:", e)
+except KeyError:
+    print("Key 'qualities' or its subkeys not found in the JSON data.")
+except (ValueError, IndexError):
+    print("Error parsing JSON or accessing array index.")
