3e22940547d56195bc84b74ada16b8e84090b34f
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2023-08-24 07:25:59 +0200

    Update szctv.py

--- ressources/dmotion/py/szctv/szctv.py
+++ ressources/dmotion/py/szctv/szctv.py
@@ -1,58 +1,34 @@
-#! /usr/bin/python3
-# Thanks to pshanmu3 user on github
+#!/usr/bin/python3
+
 import requests
-import os
-import sys
-
-proxies = {}
-if len(sys.argv) == 2:
-    proxies = {
-                'http' : sys.argv[1],
-                'https' : sys.argv[1]
-              }
-
-na = 'https://raw.githubusercontent.com/naveenland4/UTLive/main/assets/info.m3u8'
-def grab(line):
-    try:
-        _id = line.split('/')[4]
-        response = s.get(f'https://www.dailymotion.com/player/metadata/video/{_id}', proxies=proxies).json()['qualities']['auto'][0]['url']
-        m3u = s.get(response, proxies=proxies).text
-        m3u = m3u.strip().split('\n')[1:]
-        d = {}
-        cnd = True
-        for item in m3u:
-            if cnd:
-                resolution = item.strip().split(',')[2].split('=')[1]
-                if resolution not in d:
-                    d[resolution] = []
-            else:
-                d[resolution]= item
-            cnd = not cnd
-        #print(m3u)
-        m3u = d[max(d, key=int)]    
-    except Exception as e:
-        m3u = na
-    finally:
-        print(m3u)
 
 print('#EXTM3U')
 print('#EXT-X-VERSION:3')
-print('#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=2560000')
-s = requests.Session()
-with open('ressources/dmotion/py/szctv/szctv_info.txt') as f:
-    for line in f:
-        line = line.strip()
-        if not line or line.startswith('~~'):
-            continue
-        if not line.startswith('https:'):
-            line = line.split('|')
-            ch_name = line[0].strip()
-            grp_title = line[1].strip().title()
-            tvg_logo = line[2].strip()
-            tvg_id = line[3].strip()
+print('#EXT-X-STREAM-INF:RESOLUTION=1280x720,FRAME-RATE=25.000000,BANDWIDTH=2179072,CODECS="avc1.64001f,mp4a.40.2",NAME="720"')
+
+url = 'https://www.dailymotion.com/player/metadata/video/x8i9j7s'
+try:
+    response = requests.get(url)
+    response.raise_for_status()  # Raise an exception for HTTP errors
+    video_data = response.json()
+    master_url = video_data['qualities']['auto'][0]['url']
+    
+    def get_specific_line_online(url, line_number):
+        response = requests.get(url)
+        if response.status_code == 200:
+            lines = response.text.split('\n')
+            if 1 <= line_number <= len(lines):
+                return lines[line_number - 1]
+            else:
+                return None
         else:
-            grab(line)
-            
-if 'temp.txt' in os.listdir():
-    os.system('rm temp.txt')
-    os.system('rm watch*')
+            return None
+    
+    chunks = get_specific_line_online(master_url, 15)
+    print(chunks)
+except requests.exceptions.RequestException as e:
+    print("An error occurred:", e)
+except KeyError:
+    print("Key 'qualities' or its subkeys not found in the JSON data.")
+except (ValueError, IndexError):
+    print("Error parsing JSON or accessing array index.")
