db3f8f50bdeee3486248e5e00f8e6850d5fc8853
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2023-08-25 18:31:03 +0200

    Update atv.py

--- ressources/trkvz/atv.py
+++ ressources/trkvz/atv.py
@@ -9,5 +9,6 @@ headers={
 }
 
 s = requests.Session()
-response = s.get('https://securevideotoken.tmgrup.com.tr/webtv/secure?url=https%3A%2F%2Ftrkvz.daioncdn.net%2Fatv%2Fatv.m3u8%3Fce%3D3%26app%3Dd1ce2d40-5256-4550-b02e-e73c185a314e', headers=headers).json()['Url']
-print(response)
+response = s.get('https://securevideotoken.tmgrup.com.tr/webtv/secure?url=https%3A%2F%2Ftrkvz.daioncdn.net%2Fatv%2Fatv.m3u8%3Fce%3D3%26app%3Dd1ce2d40-5256-4550-b02e-e73c185a314e', headers=headers)
+string = response.text
+print(string)
