c1b8849a0e8ea3df0ca52dd312af4128a22867a4
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2023-08-31 22:27:39 +0200

    Update f3reg.sh

--- ressources/ftv/py/f3reg.sh
+++ ressources/ftv/py/f3reg.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-replacement_value=$(grep -o 'live-regions-p\.ftven\.fr/\(.*\)/hls' ressources/ftv/py/f3reg.txt | sed 's/live-regions-p\.ftven\.fr\///;s/\/hls//')
+new_string_variable=$(grep -o 'https://live-regions-p\.ftven\.fr/\(.*\)/hls/live' ressources/ftv/py/f3reg.txt | sed 's|https://live-regions-p\.ftven\.fr/||;s|/hls/live||')
 
-sed -i "s|live-regions-p.ftven.fr/ZXhDQ4|live-regions-p.ftven.fr/$replacement_value|g" all.m3u
+sed -i "s|live-regions-p.ftven.fr/[^/]*|live-regions-p.ftven.fr/$new_string_variable|g" all.m3u
