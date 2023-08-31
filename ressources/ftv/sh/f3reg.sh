788b4d69c965fe38a0b8c465133a5cceb4796c2d
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2023-08-31 21:33:35 +0200

    Update f3reg.sh

--- ressources/ftv/sh/f3reg.sh
+++ ressources/ftv/sh/f3reg.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-curl -s "https://live-regions-p.ftven.fr/ZXh0/hls/live/2037716/F3_Paris_IDF/index.m3u8" > ressources/ftv/sh/f3reg.txt
+curl -s "https://hdfauth.ftven.fr/esi/TA?url=https://live-regions-p.ftven.fr/hls/live/2037716/F3_Paris_IDF/index.m3u8" > f3reg.txt
 
-replacement_value=$(grep -o 'ftven\.fr/\(.*\)/hls' ressources/ftv/sh/f3reg.txt | sed 's/ftven\.fr\///;s/\/hls//')
+replacement_value=$(grep -o 'ftven\.fr/\(.*\)/hls' f3reg.txt | sed 's/ftven\.fr\///;s/\/hls//')
 
 sed -i "s|ftven.fr/.*?/hls|ftven.fr/$replacement_value/hls|g" all.m3u
