1c582bdfa21c3ed34974e4cbc7fafd824dbbdd22
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2023-08-31 22:13:04 +0200

    Update f3reg.sh

--- ressources/ftv/py/f3reg.sh
+++ ressources/ftv/py/f3reg.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-replacement_value=$(wget -qO - "https://raw.githubusercontent.com/ipstreet312/freeiptv/master/ressources/ftv/py/f3reg.txt" | grep -o 'ftven\.fr/\(.*\)/hls' | sed 's/ftven\.fr\///;s/\/hls//')
+replacement_value=$(grep -o 'live-regions-p\.ftven\.fr/\(.*\)/hls' ressources/ftv/py/f3reg.txt | sed 's/live-regions-p\.ftven\.fr\///;s/\/hls//')
 
-sed -i "s|ftven.fr/.*?/hls|ftven.fr/$replacement_value/hls|g" all.m3u
-
-exit 0
+sed -i "s|live-regions-p.ftven.fr/ZXhDQ4|live-regions-p.ftven.fr/$replacement_value|g" all.m3u
