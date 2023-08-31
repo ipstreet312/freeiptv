27a17fcad088f7294a0833c2cddc477ece3a73c6
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2023-08-31 21:50:46 +0200

    Delete ressources/ftv/sh directory

--- ressources/ftv/sh/f3reg.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-wget -qO f3reg.txt "https://hdfauth.ftven.fr/esi/TA?url=https://live-regions-p.ftven.fr/hls/live/2037716/F3_Paris_IDF/index.m3u8"
-
-replacement_value=$(grep -o 'ftven\.fr/\(.*\)/hls' f3reg.txt | sed 's/ftven\.fr\///;s/\/hls//')
-
-sed -i "s|ftven.fr/.*?/hls|ftven.fr/$replacement_value/hls|g" all.m3u
