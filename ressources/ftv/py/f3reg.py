905f66618170e22274335c243990bed9f557d492
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2023-09-04 04:08:47 +0200

    Delete ressources/ftv/py/f3reg.py

--- ressources/ftv/py/f3reg.py
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/python3
-
-import requests
-
-s = requests.Session()
-response = s.get(f'https://hdfauth.ftven.fr/esi/TA?url=https://live-regions-p.ftven.fr/hls/live/2037716/F3_Paris_IDF/index.m3u8')
-string = response.text
-print(string)
