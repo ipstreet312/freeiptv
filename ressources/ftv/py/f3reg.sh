ea5a49438850f00f2e8d8e712fd280149ab8a8c6
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2023-09-04 04:08:58 +0200

    Delete ressources/ftv/py/f3reg.sh

--- ressources/ftv/py/f3reg.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-new_string_variable=$(grep -o 'https://live-regions-p\.ftven\.fr/\(.*\)/hls/live' ressources/ftv/py/f3reg.txt | sed 's|https://live-regions-p\.ftven\.fr/||;s|/hls/live||')
-
-sed -i "s|live-regions-p.ftven.fr/[^/]*|live-regions-p.ftven.fr/$new_string_variable|g" all.m3u
