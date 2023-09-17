a65958a56eb51f2279639a74b21d4a1d53418f2c
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2023-09-17 07:10:38 +0200

    Update pblsnt.sh

--- ressources/dmotion/py/pblsnt/pblsnt.sh
+++ ressources/dmotion/py/pblsnt/pblsnt.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-# Thanks to LITUATUI user on github
-cd ressources/dmotion/py/pblsnt
 
-python pblsnt.py && sed -e '/xkxbzc/ {' -e 'r pblsnt.txt' -e 'd' -e '}' -i pblsnt.m3u8
+python3 ressources/dmotion/py/pblsnt/pblsnt.py > ressources/dmotion/py/pblsnt/pblsnt.m3u8
 
 exit 0
