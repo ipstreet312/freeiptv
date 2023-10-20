22566cc04570fbebe60b4bcf45abd78e7514242f
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2023-10-20 09:02:44 +0200

    Update porto.sh

--- ressources/dmotion/py/porto/porto.sh
+++ ressources/dmotion/py/porto/porto.sh
@@ -2,6 +2,6 @@
 # Thanks to LITUATUI user on github
 cd ressources/dmotion/py/porto
 
-python nessma.py && sed -e '/x8egnb8/ {' -e 'r porto.txt' -e 'd' -e '}' -i porto.m3u8
+python porto.py && sed -e '/x8egnb8/ {' -e 'r porto.txt' -e 'd' -e '}' -i porto.m3u8
 
 exit 0
