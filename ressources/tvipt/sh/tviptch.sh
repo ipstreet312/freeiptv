48da73a1ea7f270df766743e929cc82f8e4268c7
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2023-11-12 18:51:53 +0100

    Update tviptch.sh

--- ressources/tvipt/sh/tviptch.sh
+++ ressources/tvipt/sh/tviptch.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-files=(
-  "ressources/tvipt/sh/cnnpt.m3u8"
-  "ressources/tvipt/sh/tvi.m3u8"
-  "ressources/tvipt/sh/tviint.m3u8"
-)
+
+file1="ressources/tvipt/sh/cnnpt.m3u8"
+file2="ressources/tvipt/sh/tvi.m3u8"
+file3="ressources/tvipt/sh/tviint.m3u8"
+
 for file in "${files[@]}"; do
   sed -i "s#wmsAuthSign=[^&]*#wmsAuthSign=$(wget -qO- https://services.iol.pt/matrix?userId -o /dev/null)#g" "$file"
 done
