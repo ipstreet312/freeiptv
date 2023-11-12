533f346a480dc9bea81727a59bea1bedf63c2e59
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2023-11-12 18:34:23 +0100

    Update tviptch.sh

--- ressources/tvipt/sh/tviptch.sh
+++ ressources/tvipt/sh/tviptch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sed "s#wmsAuthSign=[^&]*#wmsAuthSign=$(wget -qO- https://services.iol.pt/matrix?userId -o /dev/null)#g" ressources/tvipt/sh/cnnpt.m3u8
+sed -i "s#wmsAuthSign=[^&]*#wmsAuthSign=$(wget -qO- https://services.iol.pt/matrix?userId -o /dev/null)#g" ressources/tvipt/sh/cnnpt.m3u8
 exit 0
