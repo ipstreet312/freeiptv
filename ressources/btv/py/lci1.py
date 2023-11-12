63da2c8037f7be8e26283a44f4ba2e6a39fefa12
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2023-11-12 19:23:13 +0100

    Create lci1.py

---
+++ ressources/btv/py/lci1.py
@@ -0,0 +1,18 @@
+#! /usr/bin/python3
+
+import requests
+
+print('#EXTM3U')
+print('#EXT-X-VERSION:6')
+print('#EXT-X-STREAM-INF:BANDWIDTH=3412864,AVERAGE-BANDWIDTH=2891084,RESOLUTION=1280x720,FRAME-RATE=25.000,CODECS="avc1.4D401F,mp4a.40.2",SUBTITLES="subtitles",AUDIO="audio_0"')
+s = requests.Session()
+response = s.get(f'https://raw.githubusercontent.com/ipstreet312/freeiptv/master/ressources/btv/py/lci.m3u8')
+
+string = response.text
+new_string = string.replace("index", "index_1")
+print(new_string)
+print('#EXT-X-STREAM-INF:BANDWIDTH=1168895,AVERAGE-BANDWIDTH=1021120,RESOLUTION=640x360,FRAME-RATE=25.000,CODECS="avc1.42C01E,mp4a.40.2",SUBTITLES="subtitles",AUDIO="audio_0"')
+new2_string = string.replace("index", "index_4")
+print(new2_string)
+new3_string = string.replace("index", "index_6_0")
+print(f'#EXT-X-MEDIA:TYPE=AUDIO,GROUP-ID="audio_0",CHANNELS="2",NAME="français",LANGUAGE="fra",DEFAULT=YES,AUTOSELECT=YES,URI="{new3_string}"')
