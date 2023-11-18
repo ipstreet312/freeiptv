fbc54537bb87dd651cd8d508f600fe9dba17fae5
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2023-11-18 16:33:42 +0100

    Update fr345.sh

--- ressources/ftv/py/fr345.sh
+++ ressources/ftv/py/fr345.sh
@@ -17,8 +17,10 @@ fr3_content=$(cat "$input_file" | sed 's/France_2/France_3/g; s/hls_fr2/hls_fr3/
 fr4_content=$(cat "$input_file" | sed 's/France_2/France_4/g; s/hls_fr2/hls_fr4/g')
 fr5_content=$(cat "$input_file" | sed 's/France_2/France_5/g; s/hls_fr2/hls_fr5/g')
 noa_content=$(cat "$input_file" | sed 's/France_2/Regions_noa/g; s/hls_fr2/hls_noa/g')
+fri_content=$(cat "$input_file" | sed 's/France_2/France_Info/g; s/hls_fr2/hls_monde_frinfo/g')
 
 echo "$fr3_content" > "$output_dir/fr3.m3u8"
 echo "$fr4_content" > "$output_dir/fr4.m3u8"
 echo "$fr5_content" > "$output_dir/fr5.m3u8"
 echo "$noa_content" > "$output_dir/fnoa.m3u8"
+echo "$fri_content" > "$output_dir/frin.m3u8"
