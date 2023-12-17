53c999d4cba76f394f292766caa622879b165314
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2023-12-17 16:28:52 +0100

    Create dmdirectprx.py

---
+++ ressources/dmotion/py/dmdirect/dmdirectprx.py
@@ -0,0 +1,35 @@
+import requests
+import os
+
+proxy = '20.111.54.16:8123'
+
+def snif(line):
+    try:
+        idvideo = line.split('/')[-1]
+        url = f'https://www.dailymotion.com/player/metadata/video/{idvideo}'
+        response = requests.get(url, proxies={proxy}, timeout=15).json()
+        stream_url = response['qualities']['auto'][0]['url']
+        m3u = requests.get(stream_url, proxies={proxy}, timeout=15).text
+    except Exception as e:
+        m3u = 'https://raw.githubusercontent.com/ipstreet312/freeiptv/master/ressources/infos/barkers/info.m3u8'
+
+    return m3u
+
+output_directory = 'ressources/dmotion/py/dmdirect'
+os.makedirs(output_directory, exist_ok=True)
+
+with open('ressources/dmotion/py/dmdirect/dmidprx.txt') as f:
+    current_category = None
+    for line in f:
+        line = line.strip()
+        if not line or line.startswith('~~'):
+            continue
+        elif line.startswith('#'):
+            current_category = line[1:]
+        elif line.startswith('https://'):
+            if current_category:
+                file_name = f"{current_category.strip()}.m3u8"
+                file_path = os.path.join(output_directory, file_name)
+                m3u_content = snif(line)
+                with open(file_path, 'w') as file:
+                    file.write(m3u_content)
