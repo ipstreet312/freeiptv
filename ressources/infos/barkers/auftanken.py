67be1ee9c9ffb007e0863669edd13aeaca103775
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2023-12-30 17:04:28 +0100

    Create auftanken.py

---
+++ ressources/infos/barkers/auftanken.py
@@ -0,0 +1,27 @@
+import requests
+from bs4 import BeautifulSoup
+
+def extract_src_from_html(html_code):
+    soup = BeautifulSoup(html_code, 'html.parser')
+    script_tag = soup.find('script', string=lambda text: 'player.source =' in text)
+    src_value = None
+    if script_tag:
+        src_start = script_tag.string.find("'https://")  
+        src_end = script_tag.string.find(".m3u8'") + len(".m3u8'")
+        src_value = script_tag.string[src_start:src_end]
+    return src_value
+
+def main():
+    # Fetch HTML content from the URL
+    url = 'https://webplayer.sbctv.ch/auftanken/'
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        html_code = response.text
+        src = extract_src_from_html(html_code)
+        print("Extracted SRC value:", src)
+    else:
+        print("Failed to fetch HTML content. Status code:", response.status_code)
+
+if __name__ == "__main__":
+    main()
