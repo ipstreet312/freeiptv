43e0676950aeff4db3a280396a6749e69851c40c
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2024-01-09 16:41:17 +0100

    Update shturk.py

--- ressources/tur/shturk.py
+++ ressources/tur/shturk.py
@@ -12,30 +12,34 @@ if response.status_code == 200:
     
     if match:
         json_data = match.group(1)
+        # Replacing single quotes with double quotes to make it valid JSON
         json_data_valid = json_data.replace("'", '"')
         
-        ht_data = json.loads('{' + json_data_valid + '}')
-        ht_stream_m3u8 = ht_data.get('ht_stream_m3u8')
-        
-        if ht_stream_m3u8:
-            print(f"Found Live URL: {ht_stream_m3u8}")
-            content_response = requests.get(ht_stream_m3u8)
+        try:
+            ht_data = json.loads(json_data_valid)
+            ht_stream_m3u8 = ht_data.get('ht_stream_m3u8')
             
-            if content_response.status_code == 200:
-                content = content_response.text
-                lines = content.split("\n")
-                modified_content = ""
-            
-                for line in lines:
-                    if line.startswith("showturk"):
-                        full_url = base_url + line
-                        modified_content += full_url + "\n"
-                    else:
-                        modified_content += line + "\n"
-            
-                print(modified_content)
-        else:
-            print("Live URL not found in the content.")
+            if ht_stream_m3u8:
+                print(f"Found Live URL: {ht_stream_m3u8}")
+                content_response = requests.get(ht_stream_m3u8)
+                
+                if content_response.status_code == 200:
+                    content = content_response.text
+                    lines = content.split("\n")
+                    modified_content = ""
+                
+                    for line in lines:
+                        if line.startswith("showturk"):
+                            full_url = base_url + line
+                            modified_content += full_url + "\n"
+                        else:
+                            modified_content += line + "\n"
+                
+                    print(modified_content)
+            else:
+                print("Live URL not found in the content.")
+        except json.JSONDecodeError as e:
+            print(f"JSON decoding error: {e}")
     else:
         print("Live URL pattern not found in the content.")
 else:
