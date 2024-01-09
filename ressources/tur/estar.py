f5af6f05d91d4f0f175029bb8aabce4a49641ed9
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2024-01-09 03:56:50 +0100

    Update estar.py

--- ressources/tur/estar.py
+++ ressources/tur/estar.py
@@ -1,6 +1,7 @@
 import requests
 import re
 
+base_url = "http://mn-nl.mncdn.com/dogusdyg_eurostar/dogusdyg_eurostar.smil/"
 url = "https://www.eurostartv.com.tr/canli-izle"
 response = requests.get(url)
 
@@ -12,9 +13,22 @@ if response.status_code == 200:
         baglanti = match.group(1)
         print(f"Location: {baglanti}")
         content_response = requests.get(baglanti)
+        
         if content_response.status_code == 200:
             content = content_response.text
-            print(content)
+            lines = content.split("\n")
+            modified_content = ""
+            
+            for line in lines:
+                if line.startswith("chunklist"):
+                    full_url = base_url + line
+                    modified_content += full_url + "\n"
+                else:
+                    modified_content += line + "\n"
+            
+            print(modified_content)
+        else:
+            print("Failed to fetch content.")
     else:
         print("Live URL not found in the content.")
 else:
