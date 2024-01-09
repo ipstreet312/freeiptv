67e5f7aa99c176492aa9eb11f5a52e8719475a2c
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2024-01-09 16:12:03 +0100

    Create shturk.py

---
+++ ressources/tur/shturk.py
@@ -0,0 +1,35 @@
+import requests
+import re
+
+base_url = "https://ciner-live.ercdn.net/showturk/"
+url = "https://www.showturk.com.tr/canli-yayin/showturk"
+response = requests.get(url)
+
+if response.status_code == 200:
+    site_content = response.text
+    match = re.search(r'ht_stream_m3u8 = \'(.*?)\'', site_content)
+    
+    if match:
+        baglanti = match.group(1)
+        print(f"Location: {baglanti}")
+        content_response = requests.get(baglanti)
+        
+        if content_response.status_code == 200:
+            content = content_response.text
+            lines = content.split("\n")
+            modified_content = ""
+            
+            for line in lines:
+                if line.startswith("showturk"):
+                    full_url = base_url + line
+                    modified_content += full_url + "\n"
+                else:
+                    modified_content += line + "\n"
+            
+            print(modified_content)
+        else:
+            print("Failed to fetch content.")
+    else:
+        print("Live URL not found in the content.")
+else:
+    print("Failed to fetch the website content.")
