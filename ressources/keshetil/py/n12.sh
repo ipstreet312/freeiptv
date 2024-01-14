d6c9ac72124c2d4ddacb3ff27c50cc0fb283d9f7
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2024-01-14 20:36:08 +0100

    Update n12.sh

--- ressources/keshetil/py/n12.sh
+++ ressources/keshetil/py/n12.sh
@@ -14,5 +14,9 @@ if [ ! -d "$output_dir" ]; then
 fi
 
 n12_content=$(cat "$input_file" | sed 's/direct/n12/g; s/2033791/2103938/g')
+n12b_content=$(cat "$input_file" | sed 's#direct/hls/live/2033791/k12#n12/hls/live/2041434/n12_b#g; s/_2200/_850/g')
+k12cc_content=$(cat "$input_file" | sed 's/k12/k12cc/g; s/2033791/2035325/g')
 
 echo "$n12_content" > "$output_dir/n12.m3u8"
+echo "$n12b_content" > "$output_dir/n12b.m3u8"
+echo "$k12cc_content" > "$output_dir/k12cc.m3u8"
