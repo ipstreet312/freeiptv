fd6f251eb5ee04db0769decd96837fda38252950
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2024-01-15 07:54:36 +0100

    Update allgr.py

--- ressources/infos/barkers/allgr.py
+++ ressources/infos/barkers/allgr.py
@@ -25,9 +25,10 @@ if input_text is not None:
 
         if 'group-title=' not in line:
             if current_group_title is not None:
-                split_line = line.split(',', 1)
-                if len(split_line) >= 2:
-                    line = split_line[0] + f' group-title="{current_group_title}",' + split_line[1]
+                url_start = line.find('http')
+                if url_start != -1:
+                    url = line[url_start:]
+                    line = line[:url_start] + f' group-title="{current_group_title}",' + url
 
         output_lines.append(line)
 
