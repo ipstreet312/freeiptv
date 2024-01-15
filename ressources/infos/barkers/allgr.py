12924d658fcf3a30c983cb3451911a4378b29c07
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2024-01-15 07:20:50 +0100

    Create allgr.py

---
+++ ressources/infos/barkers/allgr.py
@@ -0,0 +1,38 @@
+import requests
+
+def fetch_content_from_url(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        return response.text
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching content from URL: {e}")
+        return None
+
+url = "https://raw.githubusercontent.com/ipstreet312/freeiptv/master/all.m3u"
+input_text = fetch_content_from_url(url)
+
+if input_text is not None:
+    output_lines = []
+    current_group_title = None
+
+    for line in input_text.split('\n'):
+        if line.startswith('#EXTINF:0'):
+            group_title_start = line.find('group-title="')
+            if group_title_start != -1:
+                group_title_end = line.rfind('"')
+                current_group_title = line[group_title_start + 13: group_title_end]
+
+        if 'group-title=' not in line:
+            if current_group_title is not None:
+                line = line.rsplit(',', 1)[0] + f' group-title="{current_group_title}",' + line.rsplit(',', 1)[1]
+
+        output_lines.append(line)
+
+    output_text = '\n'.join(output_lines)
+
+    output_file_path = "allgr.m3u"
+    with open(output_file_path, "w") as output_file:
+        output_file.write(output_text)
+
+    print(output_file_path)
