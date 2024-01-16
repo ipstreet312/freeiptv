2593f787f66111aa7b65812333ae2896008b7870
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2024-01-16 04:11:12 +0100

    Update c81.py

--- ressources/dmotion/py/c8/c81.py
+++ ressources/dmotion/py/c8/c81.py
@@ -12,21 +12,28 @@ print('#EXT-X-STREAM-INF:RESOLUTION=848x477,FRAME-RATE=50.000000,BANDWIDTH=16670
 
 url = "http://s2.callofliberty.fr/direct/C8/master.m3u8"
 
-response = requests.get(url)
+try:
+    response = requests.get(url)
 
-if response.status_code == 200:
-    lines = response.text.splitlines()
-    if len(lines) >= 5:
-        third_line = lines[2]
+    if response.status_code == 200:
+        lines = response.text.splitlines()
+        if len(lines) >= 5:
+            third_line = lines[2]
+        else:
+            print("The file has less than 5 lines.")
+            exit(1)
     else:
-        print("The file has less than 5 lines.")
-else:
-    print("Failed to fetch the content. Status code:", response.status_code)
+        print("Failed to fetch the content. Status code:", response.status_code)
+        exit(1)
 
-url_part = third_line.split('"')[1]
+    url_part = third_line.split('"')[1]
 
-start_index = url_part.find('https://')
-end_index = url_part.find('m3u8')
+    start_index = url_part.find('https://')
+    end_index = url_part.find('m3u8')
 
-required_url_segment = url_part[start_index:end_index]
-print(required_url_segment + "m3u8")
+    required_url_segment = url_part[start_index:end_index]
+    print(required_url_segment + "m3u8")
+
+except Exception as e:
+    print("An error occurred:", e)
+    exit(1)
