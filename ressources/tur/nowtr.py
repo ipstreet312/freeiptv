d8b73b70c2a739322e701acf5d530dcc0c1cf3f2
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2024-01-28 13:44:26 +0100

    Update nowtr.py

--- ressources/tur/nowtr.py
+++ ressources/tur/nowtr.py
@@ -10,8 +10,9 @@ html_content = response.text
 soup = BeautifulSoup(html_content, 'html.parser')
 
 script_tag = soup.find('script', {'type': 'text/javascript'})
+print("Script Content:", script_tag.text)
 
-url_match = re.search(r"daiUrl\s*:\s*'([^']+)'", script_tag.text)
+url_match = re.search(r"daiUrl\s*:\s*'([^']+)'", script_tag.text, re.DOTALL)
 if url_match:
     dai_url = url_match.group(1)
     print("Retrieved URL:", dai_url)
