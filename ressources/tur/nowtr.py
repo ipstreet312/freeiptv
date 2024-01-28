7201771448d05c7274c9974ba60941ab2ff5f1c6
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2024-01-28 13:32:37 +0100

    Create nowtr.py

---
+++ ressources/tur/nowtr.py
@@ -0,0 +1,19 @@
+import re
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://www.fox.com.tr/canli-yayin"
+
+response = requests.get(url)
+html_content = response.text
+
+soup = BeautifulSoup(html_content, 'html.parser')
+
+script_tag = soup.find('script', {'type': 'text/javascript'})
+
+url_match = re.search(r"daiUrl\s*:\s*'([^']+)'", script_tag.text)
+if url_match:
+    dai_url = url_match.group(1)
+    print("Retrieved URL:", dai_url)
+else:
+    print("URL not found in the script.")
