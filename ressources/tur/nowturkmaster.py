d6d13410686d76bfc15ed32885279c8943ba7723
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2024-02-03 19:42:29 +0100

    Update nowturkmaster.py

--- ressources/tur/nowturkmaster.py
+++ ressources/tur/nowturkmaster.py
@@ -7,5 +7,5 @@ headers = {
 }
 
 master_url = "https://tvcdn.onrender.com/iptv-query?url=https://www.fox.com.tr/canli-yayin?m3u8"
-response = requests.head(master_url, headers=headers)
+response = requests.head(master_url, headers=headers, allow_redirects=True)
 print(response)
