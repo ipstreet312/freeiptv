10d8f8e9b1b83b2425ffffedba8dda62693de3cf
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2024-02-03 19:44:44 +0100

    Update nowturkmaster.py

--- ressources/tur/nowturkmaster.py
+++ ressources/tur/nowturkmaster.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
 import requests
 
-headers = {
-    'Connection': 'keep-alive',
-    'Content-Type': 'application/x-mpegURL'
-}
-
-master_url = "https://tvcdn.onrender.com/iptv-query?url=https://www.fox.com.tr/canli-yayin?m3u8"
-response = requests.head(master_url, headers=headers, allow_redirects=True)
-print(response)
+s = requests.Session()
+response = s.get('https://tvcdn.onrender.com/iptv-query?url=https://www.fox.com.tr/canli-yayin?m3u8')
+print(response.text)
