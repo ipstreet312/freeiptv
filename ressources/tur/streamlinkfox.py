3de9ef8970dfe10183dad83afb8036c21950433e
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2024-02-04 13:59:02 +0100

    Update streamlinkfox.py

--- ressources/tur/streamlinkfox.py
+++ ressources/tur/streamlinkfox.py
@@ -1,5 +1,12 @@
 import streamlink
 streams = streamlink.streams('https://www.fox.com.tr/canli-yayin')
-master_stream = streams['best'][1]
-master_url = master_stream.url
-print(master_url)
+best_quality_stream = streams.get('best')
+if best_quality_stream:
+    best_quality_urls = best_quality_stream.url
+    if isinstance(best_quality_urls, list) and len(best_quality_urls) >= 2:
+        second_url_of_best = best_quality_urls[1]
+        print(second_url_of_best)
+    else:
+        print("The 'best' quality does not have at least two URLs.")
+else:
+    print("Either 'best' quality does not exist or it is not an HLSStream instance.")
