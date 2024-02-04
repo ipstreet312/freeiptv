8db07513dfbdf99603cb1c0fb8a063d9b79be0f5
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2024-02-04 14:08:50 +0100

    Update streamlinkfox.py

--- ressources/tur/streamlinkfox.py
+++ ressources/tur/streamlinkfox.py
@@ -3,10 +3,14 @@ streams = streamlink.streams('https://www.fox.com.tr/canli-yayin')
 best_quality_stream = streams.get('best')
 if best_quality_stream:
     best_quality_urls = best_quality_stream.url
-    if isinstance(best_quality_urls, list) and len(best_quality_urls) >= 2:
-        second_url_of_best = best_quality_urls[1]
-        print(second_url_of_best)
+    if isinstance(best_quality_urls, list):
+        for url in best_quality_urls:
+            if url.startswith('https://foxtv.daioncdn.net/foxtv/foxtv.m3u8'):
+                print(url)
+                break
+        else:
+            print("No matching URL found in 'best' quality.")
     else:
-        print("The 'best' quality does not have at least two URLs.")
+        print("The 'best' quality does not have a list of URLs.")
 else:
     print("Either 'best' quality does not exist or it is not an HLSStream instance.")
