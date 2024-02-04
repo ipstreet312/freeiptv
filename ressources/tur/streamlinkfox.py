c4ef7e41629430c8abfe16161dc5f6b6c82b894c
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2024-02-04 13:53:08 +0100

    Update streamlinkfox.py

--- ressources/tur/streamlinkfox.py
+++ ressources/tur/streamlinkfox.py
@@ -1,5 +1,5 @@
 import streamlink
 streams = streamlink.streams('https://www.fox.com.tr/canli-yayin')
-master_stream = streams['best']
+master_stream = streams['best'][1]
 master_url = master_stream.url
 print(master_url)
