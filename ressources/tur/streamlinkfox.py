0e698fca7ad3398c5ef642f606b2a06c8748f8b4
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2024-02-04 14:14:51 +0100

    Update streamlinkfox.py

--- ressources/tur/streamlinkfox.py
+++ ressources/tur/streamlinkfox.py
@@ -1,24 +1,3 @@
 import streamlink
-
-streams = streamlink.streams('https://www.fox.com.tr/canli-yayin')
-best_quality_stream = streams.get('best')
-
-if best_quality_stream:
-    best_quality_urls = best_quality_stream.url
-
-    if isinstance(best_quality_urls, list):
-        for url in best_quality_urls:
-            if url.startswith('https://foxtv.daioncdn.net/foxtv/foxtv.m3u8'):
-                print(url)
-                break
-        else:
-            print("No matching URL found in 'best' quality.")
-    else:
-        if best_quality_urls.startswith('https://foxtv.daioncdn.net/foxtv/foxtv.m3u8'):
-            print(best_quality_urls)
-        else:
-            print("The 'best' quality URL does not match the expected pattern.")
-else:
-    print("Either 'best' quality does not exist or it is not an HLSStream instance.")
-
-
+streams = streamlink.streams('https://www.fox.com.tr/canli-yayin', 'best')
+print(streams)
