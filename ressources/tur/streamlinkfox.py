94e9232243bd18c009759607098420f078b33c78
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2024-02-04 13:40:50 +0100

    Update streamlinkfox.py

--- ressources/tur/streamlinkfox.py
+++ ressources/tur/streamlinkfox.py
@@ -1,7 +1,3 @@
 import streamlink
-your_dictionary = {
-    'best': ['your_best_url_1', 'your_best_url_2'],
-}
-streams = streamlink.streams('https://www.fox.com.tr/canli-yayin')
-master_url = streams[your_dictionary['best'][1]]
+streams = streamlink.streams('https://www.fox.com.tr/canli-yayin', M3U8)
 print(master_url)
