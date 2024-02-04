7a7692ef5d2c8548d9a822452f31c5a27307a17e
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2024-02-04 13:14:37 +0100

    Update streamlinkfox.py

--- ressources/tur/streamlinkfox.py
+++ ressources/tur/streamlinkfox.py
@@ -1,3 +1,3 @@
 import streamlink
-streams = streamlink.streams('https://www.fox.com.tr/canli-yayin')
+streams = streamlink.streams('https://www.fox.com.tr/canli-yayin', multivariant=M3U8)
 print(streams)
