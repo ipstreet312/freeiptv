63809de4ba75cfb5ad733602b410eba1e64a2f41
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2024-02-12 21:45:13 +0100

    Create kuw2.py

---
+++ ressources/kuw/kuw2.py
@@ -0,0 +1,8 @@
+import streamlink
+import requests
+streams = streamlink.streams('https://www.media.gov.kw/LiveTV.aspx?PanChannel=KTV2')
+master = streams["best"].multivariant.uri
+s = requests.Session()
+response = s.get(master)
+content = response.text
+print(content)
