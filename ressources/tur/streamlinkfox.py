7665d7ecdabf4641662ef0b29887e2e7168ceb44
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2024-02-12 20:37:26 +0100

    Update streamlinkfox.py

--- ressources/tur/streamlinkfox.py
+++ ressources/tur/streamlinkfox.py
@@ -3,5 +3,5 @@ import streamlink
 logging.basicConfig(level=logging.DEBUG)
 session = streamlink.Streamlink()
 session.set_option("http-verify", False)
-streams = session.streams('https://www.fox.com.tr/canli-yayin')
+streams = session.streams('https://www.nowtv.com.tr/canli-yayin')
 print(streams["best"].multivariant.uri)
