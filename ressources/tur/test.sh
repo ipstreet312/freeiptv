c7cdee66504869e4cb03a59a3ff847274dcdac99
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2024-02-13 16:02:49 +0100

    Update test.sh

--- ressources/tur/test.sh
+++ ressources/tur/test.sh
@@ -3,5 +3,5 @@
 erstrm_content=$(curl -s "${erstrm}")
 dastrm_content=$(curl -s "${dastrm}")
 
-sed -i "s|https://nowtv-live-ad.ercdn.net.*|${erstrm_content}|g" test.m3u
-sed -i "s|https://nowtv.daioncdn.net.*|${dastrm_content}|g" test.m3u
+sed -i "s|https://nowtv-live-ad.ercdn.net.*|${erstrm_content}|g" ressources/tur/test.m3u
+sed -i "s|https://nowtv.daioncdn.net.*|${dastrm_content}|g" ressources/tur/test.m3u
