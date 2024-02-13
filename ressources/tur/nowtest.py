a844641ee46eff53628e75b8f8714b9294f6561c
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2024-02-13 15:27:25 +0100

    Delete ressources/tur/nowtest.py

--- ressources/tur/nowtest.py
+++
@@ -1,6 +0,0 @@
-import streamlink
-streams = streamlink.streams('https://www.nowtv.com.tr/canli-yayin')
-erstrm = streams["best"].multivariant.uri
-dastrm = erstrm.replace("nowtv-live-ad.ercdn.net/nowtv/playlist.m3u8", "nowtv.daioncdn.net/nowtv/nowtv.m3u8")
-print(erstrm)
-print(dastrm)
