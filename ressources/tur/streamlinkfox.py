5bfcd58dfef444fb0c8c3c700caf2acaed8cf5f5
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2024-02-13 05:56:03 +0100

    Update streamlinkfox.py

--- ressources/tur/streamlinkfox.py
+++ ressources/tur/streamlinkfox.py
@@ -6,12 +6,4 @@ session = Streamlink()
 url = 'https://www.nowtv.com.tr/canli-yayin'
 plugin = FoxTR(session, url)
 streams = plugin._get_streams()
-
-if streams:
-    best_stream = streams.get('best')
-    if best_stream:
-        print(best_stream.url)
-    else:
-        print("No best quality stream found.")
-else:
-    print("No streams found.")
+print (streams)
