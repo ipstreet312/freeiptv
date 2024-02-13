cea28f90e17df889ce3d7c53b2c97d6b255af012
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2024-02-13 16:10:28 +0100

    Delete ressources/tur/test.sh

--- ressources/tur/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-erstrm_content=$(curl -s "$1")
-dastrm_content=$(curl -s "$2")
-
-sed -i "s|https://nowtv-live-ad.ercdn.net.*|${erstrm_content}|g" ressources/tur/test.m3u
-sed -i "s|https://nowtv.daioncdn.net.*|${dastrm_content}|g" ressources/tur/test.m3u
