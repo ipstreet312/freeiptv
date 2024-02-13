4463955f3dea20d57758e7bd0a8739eca042c982
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2024-02-13 13:19:35 +0100

    Update nowtvtr.py

--- ressources/tur/nowtvtr.py
+++ ressources/tur/nowtvtr.py
@@ -1,11 +1,4 @@
 import streamlink
-
-def main():
-    streams = streamlink.streams('https://www.nowtv.com.tr/canli-yayin')
-    erstrm = streams["best"].multivariant.uri
-    donstrm = erstrm.replace('nowtv-live-ad.ercdn.net/nowtv/playlist', 'nowtv.daioncdn.net/nowtv/nowtv')
-    print(erstrm)
-    print(donstrm)
-
-if __name__ == "__main__":
-    main()
+streams = streamlink.streams('https://www.nowtv.com.tr/canli-yayin')
+erstrm = streams["best"].multivariant.uri
+print(erstrm)
