2a175244c20e4165ad2affc01ed65e5db35a4f9c
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2024-02-13 05:44:15 +0100

    Update streamlinkfox.py

--- ressources/tur/streamlinkfox.py
+++ ressources/tur/streamlinkfox.py
@@ -1,3 +1,3 @@
-from streamlink.plugins.foxtr import foxtr
-streams = foxtr.streams('https://www.nowtv.com.tr/canli-yayin')
+from streamlink.plugins.foxtr import FoxTR
+streams = FoxTR().streams('https://www.nowtv.com.tr/canli-yayin')
 print(streams["best"].multivariant.uri)
