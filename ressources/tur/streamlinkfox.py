2c5d034dce228243aa85efa5738ee8d61d0ac726
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2024-02-13 06:49:12 +0100

    Delete ressources/tur/streamlinkfox.py

--- ressources/tur/streamlinkfox.py
+++
@@ -1,9 +0,0 @@
-from streamlink.plugins.foxtr import FoxTR
-from streamlink.stream import HLSStream
-from streamlink.session import Streamlink
-
-session = Streamlink()
-url = 'https://www.nowtv.com.tr/canli-yayin'
-plugin = FoxTR(session, url)
-streams = plugin._get_streams()
-print (streams)
