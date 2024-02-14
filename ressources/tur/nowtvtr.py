f700cb140b34459674cd2f928d446ca13f0e8f3e
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2024-02-14 20:45:53 +0100

    Update nowtvtr.py

--- ressources/tur/nowtvtr.py
+++ ressources/tur/nowtvtr.py
@@ -1,4 +1,5 @@
 import streamlink
+streamlink.set_option("ipv4", True)
 streams = streamlink.streams('https://www.nowtv.com.tr/canli-yayin')
 erstrm = streams["best"].multivariant.uri
 print(erstrm)
