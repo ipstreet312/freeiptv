73cdc5b346af1b1d21baf2197efb978583861b53
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2024-02-21 23:27:50 +0100

    Update testdm.py

--- ressources/dmotion/py/streamlink/testdm.py
+++ ressources/dmotion/py/streamlink/testdm.py
@@ -1,6 +1,6 @@
 import streamlink
 import requests
-streams = streamlink.streams('https://www.dailymotion.com/video/x5idxor')
+streams = streamlink.streams('https://www.dailymotion.com/video/k2IuM11CZakq4ZvrUkv')
 master = streams["best"].multivariant.uri
 s = requests.Session()
 response = s.get(master)
