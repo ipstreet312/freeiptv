5d8d5e2fef6cb0d28101c4a042ad6c5f83b68f8c
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2024-02-21 17:34:21 +0100

    Update and rename testdm.py to nessmastdm.py

--- ressources/dmotion/py/streamlink/nessmastdm.py
+++ ressources/dmotion/py/streamlink/nessmastdm.py
@@ -1,6 +1,6 @@
 import streamlink
 import requests
-streams = streamlink.streams('https://www.dailymotion.com/video/x5gv5v0')
+streams = streamlink.streams('https://www.dailymotion.com/video/x7va0xb')
 master = streams["best"].multivariant.uri
 s = requests.Session()
 response = s.get(master)
