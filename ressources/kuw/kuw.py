24c7f169db680f716f99f9859231bed97af378d7
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2024-02-23 00:23:16 +0100

    Update kuw.py

--- ressources/kuw/kuw.py
+++ ressources/kuw/kuw.py
@@ -1,7 +1,7 @@
 import streamlink
 import requests
 session = streamlink.session.Streamlink(options={"ipv4": True})
-streams = session.streams('http://media.gov.kw/LiveTV.aspx?PanChannel=KTV1')
+streams = session.streams('https://media.gov.kw/LiveTV.aspx?PanChannel=KTV1')
 master = streams["best"].multivariant.uri
 s = requests.Session()
 response = s.get(master)
