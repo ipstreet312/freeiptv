002e3785b6172add1a2f0d20ba42a6444e427135
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2024-02-23 13:20:32 +0100

    Update kuw.py

--- ressources/kuw/kuw.py
+++ ressources/kuw/kuw.py
@@ -1,6 +1,6 @@
 import streamlink
 import requests
-streams = streamlink.streams('https://media.gov.kw/LiveTV.aspx?PanChannel=KTV2')
+streams = streamlink.streams('https://player.mangomolo.com/v1/live?id=MTk1&channelid=MzU2&countries=bnVsbA==&w=100%25&h=100%25&autoplay=true&filter=none&signature=6dbebfb531d4dfb9a798f6309ea9260c&app_id=43')
 master = streams["best"].multivariant.uri
 s = requests.Session()
 response = s.get(master)
