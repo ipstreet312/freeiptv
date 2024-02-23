87ea0cb4278b6fce1b95c4512827f56cf12c8fff
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2024-02-23 13:21:51 +0100

    Update kuw.py

--- ressources/kuw/kuw.py
+++ ressources/kuw/kuw.py
@@ -1,6 +1,6 @@
 import streamlink
 import requests
-streams = streamlink.streams('https://player.mangomolo.com/v1/live?id=MTk1&channelid=MzU2&countries=bnVsbA==&w=100%25&h=100%25&autoplay=true&filter=none&signature=6dbebfb531d4dfb9a798f6309ea9260c&app_id=43')
+streams = streamlink.streams('https://player.mangomolo.com/v1/live?id=MTk1&channelid=MzU1&countries=bnVsbA==&w=100%25&h=100%25&autoplay=true&filter=none&signature=9ea6c8ed03b8de6e339d6df2b0685f25&app_id=43')
 master = streams["best"].multivariant.uri
 s = requests.Session()
 response = s.get(master)
