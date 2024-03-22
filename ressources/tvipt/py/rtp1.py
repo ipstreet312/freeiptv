57c5fbdfc8c8c68b5e79a160e4ff2b0f7d99890b
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2024-03-22 15:06:00 +0100

    Update rtp1.py

--- ressources/tvipt/py/rtp1.py
+++ ressources/tvipt/py/rtp1.py
@@ -1,4 +1,4 @@
 import streamlink
 streams = streamlink.streams('https://www.rtp.pt/play/direto/rtp1')
-rtp1 = streams["best"].multivariant.uri
-print(rtp1)
+master1 = streams["best"].multivariant.uri
+print(master1)
