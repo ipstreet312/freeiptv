a49a93dd6dc8d4aa4612d63639891a7f76f2b97b
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2024-03-22 15:46:17 +0100

    Update rtp2.py

--- ressources/tvipt/py/rtp2.py
+++ ressources/tvipt/py/rtp2.py
@@ -1,6 +1,6 @@
 import streamlink
 import requests
-streams = streamlink.streams('https://www.nowtv.com.tr/canli-yayin')
+streams = streamlink.streams('https://www.rtp.pt/play/direto/rtp2')
 master2 = streams["best"].url
 content2 = requests.get(master2)
 print(content2.text)
