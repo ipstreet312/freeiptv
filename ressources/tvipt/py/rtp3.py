179dd37555b19f0ba0c323f2dcdee5c19266ee9e
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2024-03-22 15:21:32 +0100

    Update rtp3.py

--- ressources/tvipt/py/rtp3.py
+++ ressources/tvipt/py/rtp3.py
@@ -2,7 +2,5 @@ import streamlink
 import requests
 streams = streamlink.streams('https://www.rtp.pt/play/direto/rtp3')
 master3 = streams["best"].url
-s = requests.Session()
-response = s.get(master3)
-content3 = response.text
-print(content3)
+content3 = requests.get(master3)
+print(content3.text)
