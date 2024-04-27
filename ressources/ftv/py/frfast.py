c66913405aaf03f45c17db254fba0c295bf4be95
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2024-04-27 14:05:47 +0300

    Update frfast.py

--- ressources/ftv/py/frfast.py
+++ ressources/ftv/py/frfast.py
@@ -21,16 +21,22 @@ def generate_frser_m3u8(original_url, output_file):
     with open(output_file, "a") as f:
         print(f'#EXT-X-MEDIA:TYPE=AUDIO,GROUP-ID="audio_1000017564_128",LANGUAGE="fr",NAME="fr",DEFAULT=YES,AUTOSELECT=YES,URI="{newser2_string}"', file=f)
 
-def generate_frdoc_m3u8(original_url, replacement_url, output_file):
+def generate_frdoc_m3u8(original_url, replacement_url, output_file):  # Updated function signature
     string = fetch_response(f'https://hdfauth.ftven.fr/esi/TA?url={original_url}/manifest.m3u8')
 
-    newdoc_string = string.replace(original_url, replacement_url).replace("manifest", "video_7201280_p_0")
+    # Replace the URL part only
+    newdoc_string = string.replace(f"{original_url}/manifest.m3u8", replacement_url)
+
+    # Then replace "manifest" with "video_7201280_p_0"
+    newdoc_string = newdoc_string.replace("manifest", "video_7201280_p_0")
+    
     with open(output_file, "w") as f:
         print('#EXTM3U', file=f)
         print('#EXT-X-VERSION:3', file=f)
         print('#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=3022199,CODECS="avc1.64001F, mp4a.40.2", RESOLUTION=1280x720, SUBTITLES="subs", AUDIO="audio_1000017564_128"', file=f)
         print(newdoc_string, file=f)
-    newdoc2_string = newdoc_string.replace("manifest", "A_audio_1000017564_128_fr")  # Apply the second replacement to the updated string
+    
+    newdoc2_string = newdoc_string.replace(original_url, replacement_url).replace("manifest", "A_audio_1000017564_128_fr")
     with open(output_file, "a") as f:
         print(f'#EXT-X-MEDIA:TYPE=AUDIO,GROUP-ID="audio_1000017564_128",LANGUAGE="fr",NAME="fr",DEFAULT=YES,AUTOSELECT=YES,URI="{newdoc2_string}"', file=f)
 
@@ -43,4 +49,4 @@ if __name__ == "__main__":
     replacement_url = "https://live-thema.ftven.fr/docs/735e9260-bb63-11ee-a1a7-0200170265fd_0_HLS-francedomtom"
     
     generate_frser_m3u8(original_url, sys.argv[1])
-    generate_frdoc_m3u8(original_url, replacement_url, sys.argv[2])
+    generate_frdoc_m3u8(original_url, replacement_url, sys.argv[2])  # Pass replacement_url as an argument
