5eff682f08cf649a06a8ef8b6aec421f4dc94679
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2024-04-27 13:56:08 +0300

    Update frfast.py

--- ressources/ftv/py/frfast.py
+++ ressources/ftv/py/frfast.py
@@ -21,7 +21,7 @@ def generate_frser_m3u8(original_url, output_file):
     with open(output_file, "a") as f:
         print(f'#EXT-X-MEDIA:TYPE=AUDIO,GROUP-ID="audio_1000017564_128",LANGUAGE="fr",NAME="fr",DEFAULT=YES,AUTOSELECT=YES,URI="{newser2_string}"', file=f)
 
-def generate_frdoc_m3u8(original_url, replacement_url, output_file):  # Updated function signature
+def generate_frdoc_m3u8(original_url, replacement_url, output_file):
     string = fetch_response(f'https://hdfauth.ftven.fr/esi/TA?url={original_url}/manifest.m3u8')
 
     newdoc_string = string.replace(original_url, replacement_url).replace("manifest", "video_7201280_p_0")
@@ -30,7 +30,7 @@ def generate_frdoc_m3u8(original_url, replacement_url, output_file):  # Updated 
         print('#EXT-X-VERSION:3', file=f)
         print('#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=3022199,CODECS="avc1.64001F, mp4a.40.2", RESOLUTION=1280x720, SUBTITLES="subs", AUDIO="audio_1000017564_128"', file=f)
         print(newdoc_string, file=f)
-    newdoc2_string = newdoc_string.replace(original_url, replacement_url).replace("manifest", "A_audio_1000017564_128_fr")
+    newdoc2_string = newdoc_string.replace("manifest", "A_audio_1000017564_128_fr")  # Apply the second replacement to the updated string
     with open(output_file, "a") as f:
         print(f'#EXT-X-MEDIA:TYPE=AUDIO,GROUP-ID="audio_1000017564_128",LANGUAGE="fr",NAME="fr",DEFAULT=YES,AUTOSELECT=YES,URI="{newdoc2_string}"', file=f)
 
@@ -43,4 +43,4 @@ if __name__ == "__main__":
     replacement_url = "https://live-thema.ftven.fr/docs/735e9260-bb63-11ee-a1a7-0200170265fd_0_HLS-francedomtom"
     
     generate_frser_m3u8(original_url, sys.argv[1])
-    generate_frdoc_m3u8(original_url, replacement_url, sys.argv[2])  # Pass replacement_url as an argument
+    generate_frdoc_m3u8(original_url, replacement_url, sys.argv[2])
