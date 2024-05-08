35ed0c7cf1e5f845edcfcd6cd2d2485d41cff28f
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2024-05-08 20:44:42 +0300

    Create frjo24.py

---
+++ ressources/ftv/py/frjo24.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import requests
+import re
+
+print('#EXTM3U')
+print('#EXT-X-VERSION:3')
+print('#EXT-X-STREAM-INF:BANDWIDTH=2855600,AVERAGE-BANDWIDTH=2479236,RESOLUTION=1280x720,FRAME-RATE=25.000,CODECS="avc1.640020,mp4a.40.2"')
+
+s = requests.Session()
+response = s.get(f'https://hdfauth.ftven.fr/esi/TA?url=https%3A%2F%2Flive-olympics.ftven.fr%2Fparis2024-francedomtom%2Fmaster.m3u8')
+
+string = response.text
+response2 = s.get(string)
+
+new_string = string.replace("master", "b0EF_720p/playlist")
+print(new_string)
