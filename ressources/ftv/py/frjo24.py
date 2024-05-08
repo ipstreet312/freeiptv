e88b11274bb3780405a1ca169d9e236b386cca80
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2024-05-08 20:49:11 +0300

    Update frjo24.py

--- ressources/ftv/py/frjo24.py
+++ ressources/ftv/py/frjo24.py
@@ -9,9 +9,7 @@ print('#EXT-X-STREAM-INF:BANDWIDTH=2855600,AVERAGE-BANDWIDTH=2479236,RESOLUTION=
 
 s = requests.Session()
 response = s.get(f'https://hdfauth.ftven.fr/esi/TA?url=https%3A%2F%2Flive-olympics.ftven.fr%2Fparis2024-francedomtom%2Fmaster.m3u8')
-
 string = response.text
-response2 = s.get(string)
 
 new_string = string.replace("master", "b0EF_720p/playlist")
 print(new_string)
