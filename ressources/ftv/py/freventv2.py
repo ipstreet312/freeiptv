c981f61b974c6a0b98f02a2b6d8d23c0e53a3b1a
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2024-06-11 06:52:51 +0200

    Update freventv2.py

--- ressources/ftv/py/freventv2.py
+++ ressources/ftv/py/freventv2.py
@@ -16,6 +16,7 @@ def generate_frjo24v2_m3u8(string, output_file):
         print('#EXT-X-STREAM-INF:BANDWIDTH=2855600,AVERAGE-BANDWIDTH=2479236,RESOLUTION=1280x720,FRAME-RATE=25.000,CODECS="avc1.640020,mp4a.40.2"', file=f)
         print(frjo24v2_string, file=f)
 
+"""
 def generate_frfescanv2_m3u8(string, output_file):
     fescanv2_string = string.replace("live-olympics", "live-event").replace("paris2024-francedomtom", "out/v1/65fb695a25404e08944943d011abca75").replace("master", "index_france-domtom_26")
     with open(output_file, "w") as f:
@@ -23,13 +24,13 @@ def generate_frfescanv2_m3u8(string, output_file):
         print('#EXT-X-VERSION:3', file=f)
         print('#EXT-X-STREAM-INF:BANDWIDTH=4569457,AVERAGE-BANDWIDTH=2961824,RESOLUTION=1280x720,FRAME-RATE=25.000,CODECS="avc1.64001F,mp4a.40.2"', file=f)
         print(fescanv2_string, file=f)
+"""
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: python3 freventv2.py <output_file_frjo24v2> <output_file_frfescanv2>")
+    if len(sys.argv) != 2:
+        print("Usage: python3 freventv2.py <output_file_frjo24v2>")
         sys.exit(1)
     
     string = fetch_response(f'https://hdfauth.ftven.fr/esi/TA?url=https%3A%2F%2Flive-olympics.ftven.fr%2Fparis2024-francedomtom%2Fmaster.m3u8')
     
     generate_frjo24v2_m3u8(string, sys.argv[1])
-    generate_frfescanv2_m3u8(string, sys.argv[2])
