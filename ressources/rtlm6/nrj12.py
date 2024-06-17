9a505519b6cf95a1958178b592dc30c4ce01824a
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2024-06-17 07:12:55 +0200

    Update nrj12.py

--- ressources/rtlm6/nrj12.py
+++ ressources/rtlm6/nrj12.py
@@ -1,20 +1,22 @@
 import requests
 
 github_url = "https://raw.githubusercontent.com/Paradise-91/ParaTV/main/streams/nrj/nrj12.m3u8"
+base_url = "https://event1.nrjaudio.fm/hls/live/2038378/nrj_12/"
 
 def fetch_m3u8_content(url):
     response = requests.get(url)
     response.raise_for_status()  # Ensure bad responses if any
     lines = response.text.splitlines()
     
-    # Fetch the content of the third link
-    if len(lines) > 2:
-        third_link = lines[2]
-        third_response = requests.get(third_link)
-        third_response.raise_for_status()
-        return third_response.text
-    else:
-        raise ValueError("The file does not contain enough links.")
+    # Construct full URLs
+    updated_lines = []
+    for line in lines:
+        if line.startswith("hdntl="):
+            updated_lines.append(base_url + line)
+        else:
+            updated_lines.append(line)
+    
+    return "\n".join(updated_lines)
 
 if __name__ == "__main__":
     try:
