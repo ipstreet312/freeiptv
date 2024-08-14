8d8992f4dd0d77bd09716b23a90b17ab4e9312cd
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2024-08-14 17:10:34 +0200

    Update ntv.py

--- ressources/tur/ntv.py
+++ ressources/tur/ntv.py
@@ -1,5 +1,6 @@
 import requests
 
+base_url = "https://mn-nl.mncdn.com/dogusdyg_ntv/dogusdyg_ntv.smil/"
 initial_url = "https://dygvideo.dygdigital.com/live/hls/kralpop?m3u8"
 
 try:
@@ -7,10 +8,22 @@ try:
     response.raise_for_status()
 
     final_url = response.url
-    print(f"Final URL after redirection: {final_url}")
-
     modified_url = final_url.replace("kralpoptv", "ntv")
-    print(f"Modified URL: {modified_url}")
+
+    content_response = requests.get(modified_url)
+    content_response.raise_for_status()
+    content = content_response.text
+    
+    lines = content.split("\n")
+    modified_content = ""
+
+    for line in lines:
+        if line.startswith("chunklist"):
+            full_url = base_url + line
+            modified_content += full_url + "\n"
+        else:
+            modified_content += line + "\n"
+    print(modified_content)
 
 except requests.RequestException as e:
     print(f"An error occurred: {e}")
