c31e525559adee2f90d317362b93cc3e0244928c
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2024-08-14 16:54:24 +0200

    Update ntv.py

--- ressources/tur/ntv.py
+++ ressources/tur/ntv.py
@@ -1,30 +1,24 @@
 import requests
 
-base_url = "https://mn-nl.mncdn.com/dogusdyg_ntv/dogusdyg_ntv.smil/"
 url = "https://dygvideo.dygdigital.com/live/hls/kralpop?m3u8"
 
 try:
     response = requests.get(url)
     response.raise_for_status()
 
-    playlist_content = response.text.strip()
-    print(f"Original playlist content: {playlist_content[:500]}...")
+    playlist_url = response.text.strip()
+    print(f"Original playlist URL: {playlist_url}")
 
-    modified_playlist_content = playlist_content.replace("kralpoptv", "ntv")
+    modified_url = playlist_url.replace("kralpoptv", "ntv")
+    print(f"Modified playlist URL: {modified_url}")
 
-    print(f"Modified playlist content: {modified_playlist_content[:500]}...")
+    content_response = requests.get(modified_url)
+    content_response.raise_for_status()
 
-    modified_content = ""
-    lines = modified_playlist_content.split("\n")
+    content = content_response.text
 
-    for line in lines:
-        if line.startswith("chunklist"):
-            full_url = base_url + line
-            modified_content += full_url + "\n"
-        else:
-            modified_content += line + "\n"
-
-    print(modified_content)
+    print("Content from the modified URL:")
+    print(content)
 
 except requests.RequestException as e:
     print(f"An error occurred: {e}")
