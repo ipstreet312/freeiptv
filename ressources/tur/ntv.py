458ea8e7dbd4e123e06c79f4964cfdb8a5f658da
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2024-08-14 17:04:19 +0200

    Update ntv.py

--- ressources/tur/ntv.py
+++ ressources/tur/ntv.py
@@ -1,24 +1,16 @@
 import requests
 
-url = "https://dygvideo.dygdigital.com/live/hls/kralpop?m3u8"
+initial_url = "https://dygvideo.dygdigital.com/live/hls/kralpop?m3u8"
 
 try:
-    response = requests.get(url)
+    response = requests.get(initial_url)
     response.raise_for_status()
 
-    playlist_url = response.text.strip()
-    print(f"Original playlist URL: {playlist_url}")
+    final_url = response.url
+    print(f"Final URL after redirection: {final_url}")
 
-    modified_url = playlist_url.replace("kralpoptv", "ntv")
-    print(f"Modified playlist URL: {modified_url}")
-
-    content_response = requests.get(modified_url)
-    content_response.raise_for_status()
-
-    content = content_response.text
-
-    print("Content from the modified URL:")
-    print(content)
+    modified_url = final_url.replace("kralpoptv", "ntv")
+    print(f"Modified URL: {modified_url}")
 
 except requests.RequestException as e:
     print(f"An error occurred: {e}")
