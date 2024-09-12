66787c5748931b933b5e182af3fca905da6343d0
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2024-09-12 21:28:02 +0200

    Update ntv.py

--- ressources/tur/ntv.py
+++ ressources/tur/ntv.py
@@ -1,6 +1,6 @@
 import requests
 
-base_url = "https://mn-nl.mncdn.com/dogusdyg_ntv/dogusdyg_ntv.smil/"
+base_url = "https://mn-nl.mncdn.com/dogusdyg_ntv/"
 initial_url = "https://dygvideo.dygdigital.com/live/hls/kralpop?m3u8"
 
 try:
@@ -8,7 +8,7 @@ try:
     response.raise_for_status()
 
     final_url = response.url
-    modified_url = final_url.replace("kralpoptv", "ntv")
+    modified_url = final_url.replace("dogusdyg_kralpoptv/dogusdyg_kralpoptv.smil/playlist", "dogusdyg_ntv/live")
 
     content_response = requests.get(modified_url)
     content_response.raise_for_status()
@@ -18,7 +18,7 @@ try:
     modified_content = ""
 
     for line in lines:
-        if line.startswith("chunklist"):
+        if line.startswith("live_"):
             full_url = base_url + line
             modified_content += full_url + "\n"
         else:
