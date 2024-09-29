42f23876f1880efa6897d37db87cf0ded7a1efd8
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2024-09-29 14:55:25 +0200

    Update egetv.py

--- ressources/tur/egetv.py
+++ ressources/tur/egetv.py
@@ -1,11 +1,13 @@
 import requests
 
-url = "https://cdnet.alwaysdata.net/egetvcurl.php?m3u8"
+url = "https://cdnet.alwaysdata.net/egetvcurlecho.php?m3u8"
 
 response = requests.get(url)
 
 if response.status_code == 200:
     content = response.text
-    print(content)
+    lines = content.splitlines()
+    for line in lines:
+        print(line)
 else:
     print(f"Failed to retrieve the file. Status code: {response.status_code}")
