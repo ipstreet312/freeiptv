71c9aaefae9a2bf9b300b946435d065ed7988cb8
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2024-09-29 14:41:21 +0200

    Update egetv.py

--- ressources/tur/egetv.py
+++ ressources/tur/egetv.py
@@ -1,13 +1,11 @@
 import requests
 
-url = "https://uzunmuhalefet.serv00.net/canlitv2.php?id=841&.m3u8"
-base_url = "http://50.7.218.154:1935/canlitv/egetv.stream/"
+url = "https://cdnet.alwaysdata.net/egetvcurl.php?m3u8"
 
 response = requests.get(url)
 
 if response.status_code == 200:
     content = response.text
-    modified_content = content.replace("chunklist", base_url + "chunklist")
-    print(modified_content)
+    print(content)
 else:
     print(f"Failed to retrieve the file. Status code: {response.status_code}")
