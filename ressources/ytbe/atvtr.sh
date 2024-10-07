b8e0723704958f0cc485e45c3746be3babc34761
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2024-10-07 06:48:17 +0200

    Update atvtr.sh

--- ressources/ytbe/atvtr.sh
+++ ressources/ytbe/atvtr.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-echo $(dirname $0)
+python3  ressources/ytbe/atvtr.py > ressources/ytbe/atvtr.m3u8
 
-python3 -m pip install requests
-
-cd $(dirname $0)/ressources/ytbe
-
-python3 atvtr.py > ../atvtr.m3u8
-
-echo m3u8 grabbed
+exit 0
