2a29f796d81cffffa1038d5f593521b30460485d
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2024-11-15 03:03:35 +0100

    Update fr345.sh

--- ressources/ftv/py/fr345.sh
+++ ressources/ftv/py/fr345.sh
@@ -18,6 +18,7 @@ fr4_content=$(cat "$input_file" | sed 's/France_2/France_4/g; s/hls_fr2/hls_fr4/
 fr5_content=$(cat "$input_file" | sed 's/France_2/France_5/g; s/hls_fr2/hls_fr5/g')
 noa_content=$(cat "$input_file" | sed 's/France_2/Regions_noa/g; s/hls_fr2/hls_noa/g')
 fri_content=$(cat "$input_file" | sed 's/France_2/France_Info/g; s/hls_fr2/hls_monde_frinfo/g')
+arte_content=$(cat "$input_file" | sed 's/France_2/Arte/g; s/-p/-frmedias-p/g; s/hls_fr2/hls_arte/g')
 fr1wf_content=$(cat "$input_file" | sed 's/France_2/1er_Wallis_Futuna/g; s/hls_fr2/hls_1er_WF/g')
 fr1wfm_content=$(cat "$input_file" | sed 's/France_2/1er_Wallis_Futuna/g; s/hls_fr2/hls_monde_1er_WF/g')
 fr1spm_content=$(cat "$input_file" | sed 's/France_2/1er_Saint-Pierre_Miquelon/g; s/hls_fr2/hls_1er_SPM/g')
@@ -48,6 +49,7 @@ echo "$fr4_content" > "$output_dir/fr4.m3u8"
 echo "$fr5_content" > "$output_dir/fr5.m3u8"
 echo "$noa_content" > "$output_dir/fnoa.m3u8"
 echo "$fri_content" > "$output_dir/frin.m3u8"
+echo "$arte_content" > "$output_dir/frarte.m3u8"
 echo "$fr1wf_content" > "$output_dir/fr1wf.m3u8"
 echo "$fr1wfm_content" > "$output_dir/fr1wfm.m3u8"
 echo "$fr1spm_content" > "$output_dir/fr1spm.m3u8"
