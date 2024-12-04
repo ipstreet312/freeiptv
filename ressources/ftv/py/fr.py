616d70ed7df6569349a51b401d60a55a3832fbe6
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2024-12-04 20:21:58 +0100

    Update fr.py

--- ressources/ftv/py/fr.py
+++ ressources/ftv/py/fr.py
@@ -9,7 +9,7 @@ s = requests.Session()
 response = s.get(f'https://hdfauth.ftven.fr/esi/TA?url=https://simulcast-p.ftven.fr/simulcast/France_2/hls_fr2/index.m3u8')
 
 string = response.text
-new_string = string.replace("index", "France_2-avc1_2500000=10001")
+new_string = string.replace("index", "France_2-avc1_2500000=5")
 print(new_string)
-new2_string = string.replace("index", "France_2-mp4a_96000_fra=20000")
+new2_string = string.replace("index", "France_2-mp4a_96000_fra=1")
 print(f'#EXT-X-MEDIA:TYPE=AUDIO,GROUP-ID="audio-AACL-96",LANGUAGE="fr",NAME="Francais",DEFAULT=YES,AUTOSELECT=YES,CHANNELS="2",URI="{new2_string}"')
