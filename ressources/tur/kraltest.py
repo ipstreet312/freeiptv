5c4cce4a37f81ec8e8de29126f8b2876ce98bbb2
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2024-12-07 06:31:34 +0100

    Update kraltest.py

--- ressources/tur/kraltest.py
+++ ressources/tur/kraltest.py
@@ -1,11 +1,16 @@
 import requests
+import m3u8
 
 url = "https://dygvideo.dygdigital.com/live/hls/kralpop?m3u8"
 
-headers = {
-    "Connection": "keep-alive",
-}
+# headers = {
+#     "Connection": "keep-alive",
+# }
 
-response = requests.get(url, headers=headers)
+# response = requests.get(url, headers=headers)
+#
 
-print(response.text)
+response = requests.get(url)
+
+playlist = m3u8.loads(response.text)
+print(playlist.segments)
