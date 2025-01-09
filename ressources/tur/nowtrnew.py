b3aa73e03a94daa44234c6cefeb42efc16c2ab43
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2025-01-09 20:19:26 +0100

    Update nowtrnew.py

--- ressources/tur/nowtrnew.py
+++ ressources/tur/nowtrnew.py
@@ -1,5 +1,13 @@
 import streamlink
-session = streamlink.session.Streamlink(options={"http-no-ssl-verify": True})
+session = streamlink.Streamlink(
+    options={
+        "http-no-ssl-verify": True,
+        "http-disable-dh": True
+    }
+)
 streams = session.streams('https://www.nowtv.com.tr/canli-yayin')
 dastrm = streams["best"].multivariant.uri
 print(dastrm)
+session.set_option("http-disable-dh", True)
+
+import streamlink
