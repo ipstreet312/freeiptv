8c9755517f728400faf102aaaaea22ee456d2572
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2025-01-10 01:01:29 +0100

    Update nowtur1.py

--- ressources/tur/nowtur1.py
+++ ressources/tur/nowtur1.py
@@ -9,9 +9,9 @@ if response.status_code == 200:
     match = re.search(r"daiUrl\s*:\s*'(https?://[^\']+)'", response.text)
   
     if match:
-        dai_url = match.group(1)
-        print(f"The extracted daiUrl is: {dai_url}")
+        erstrm = match.group(1)
+        print(erstrm)
     else:
-        print("daiUrl not found in the content.")
+        print("erstrm not found in the content.")
 else:
     print(f"Failed to fetch content. HTTP Status code: {response.status_code}")
