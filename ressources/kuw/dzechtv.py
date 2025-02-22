3e4a18395bf8ee29b4f9594920becc6d6a7c0180
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2025-02-22 21:05:24 +0100

    Create dzechtv.py

---
+++ ressources/kuw/dzechtv.py
@@ -0,0 +1,16 @@
+#! /usr/bin/python3
+
+import requests
+
+s = requests.Session()
+
+link = "https://cdnet.alwaysdata.net/dzechtv.php?m3u8"
+
+headers = {
+    'Connection': 'keep-alive',
+    'Content-Type': 'application/x-mpegURL'
+}
+
+response = s.get(link, headers=headers)
+
+print(response.text)
