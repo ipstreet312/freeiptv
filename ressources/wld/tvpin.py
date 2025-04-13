e097144bd635f1f29d0bfc2d625c44ef46a0246b
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2025-04-13 14:50:04 +0200

    Update tvpin.py

--- ressources/wld/tvpin.py
+++ ressources/wld/tvpin.py
@@ -2,7 +2,6 @@ import streamlink
 
 streams = streamlink.streams('https://stream.tvp.pl/?channel_id=1455')
 erstrm = streams["best"].multivariant.uri
-print(erstrm)
 
 print("#EXTM3U")
 print("#EXT-X-VERSION:4")
