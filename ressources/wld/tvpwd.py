a48d53e9ee8dc308fc0e477e2cc067adc4645382
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2025-04-13 15:04:34 +0200

    Update tvpwd.py

--- ressources/wld/tvpwd.py
+++ ressources/wld/tvpwd.py
@@ -2,10 +2,8 @@ import streamlink
 
 streams = streamlink.streams('https://stream.tvp.pl/?channel_id=51656487')
 erstrm = streams["best"].multivariant.uri
-print(erstrm)
+
 print("#EXTM3U")
-print("#EXT-X-VERSION:4")
-print("#EXT-X-INDEPENDENT-SEGMENTS")
-print('#EXT-X-STREAM-INF:BANDWIDTH=4056800,AVERAGE-BANDWIDTH=2741200,CODECS="avc1.640020,mp4a.40.2",RESOLUTION=1024x576,FRAME-RATE=50.000,AUDIO="program_audio_0"')
-print(erstrm.replace("master.m3u8", "master_v2.m3u8"))
-print(f'#EXT-X-MEDIA:TYPE=AUDIO,URI="{erstrm.replace("master.m3u8", "master_a1.m3u8")}"')
+print("#EXT-X-VERSION:5")
+print('#EXT-X-STREAM-INF:BANDWIDTH=4858000,AVERAGE-BANDWIDTH=4416000,CODECS="mp4a.40.2,avc1.640020",RESOLUTION=1280x720,FRAME-RATE=50,VIDEO-RANGE=SDR,AUDIO="audio-aacl-211",CLOSED-CAPTIONS=NONE"')
+print(erstrm.replace("master.m3u8", "TVP_World-audio_211682_pol=211200-video=3954400.m3u8"))
