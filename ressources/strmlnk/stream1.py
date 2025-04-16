e385d947e2333c57c84a3fad198efd4126aacf5a
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2025-04-16 07:56:33 +0200

    Update stream1.py

--- ressources/strmlnk/stream1.py
+++ ressources/strmlnk/stream1.py
@@ -4,7 +4,7 @@ session = Streamlink()
 streams = session.streams('https://stream.tvp.pl/?channel_id=1455')
 
 # Try to prioritize 'dash', then 'best', or just get any stream
-for quality in ["dash", "besti"]:
+for quality in ["dash", "best"]:
     if quality in streams:
         stream = streams[quality]
         break
@@ -12,9 +12,10 @@ else:
     print("No suitable stream found.")
     exit(1)
     
-print(stream)
+"""print(stream)
+"""
 
-"""# Check if the stream is multivariant (e.g., HLS master playlist)
+# Check if the stream is multivariant (e.g., HLS master playlist)
 if hasattr(stream, "multivariant") and stream.multivariant:
     erstrm = stream.multivariant.uri
 else:
@@ -23,4 +24,4 @@ else:
 
 print(erstrm)
 
-print("Available stream qualities:", list(streams.keys()))"""
+print("Available stream qualities:", list(streams.keys()))
