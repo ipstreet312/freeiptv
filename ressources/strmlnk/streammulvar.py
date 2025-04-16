20de1004768d816b6a614d3acf6904324b4b7564
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2025-04-16 07:27:10 +0200

    Update streammulvar.py

--- ressources/strmlnk/streammulvar.py
+++ ressources/strmlnk/streammulvar.py
@@ -2,6 +2,3 @@ import streamlink
 streams = streamlink.streams('https://stream.tvp.pl/?channel_id=1455')
 beststrmmul = streams["best"].multivariant.uri
 print(beststrmmul)
-print("XXX")
-dashlmul = streams["dash"].multivariant.uri
-print(dashlmul)
