1f7355a15d74286396eb4afe1c883e77ccc2a722
ipstreet312 <60736167+ipstreet312@users.noreply.github.com>
2025-10-19 13:51:11 +0200

    Fix URL for Showmax live stream

--- ressources/tur/shmax.py
+++ ressources/tur/shmax.py
@@ -5,7 +5,7 @@ import re
 base_url = "https://ciner-live.ercdn.net/showmax/"
 
 # Page containing the player setup
-url = "http://www.showmax.com.tr/canliyayin"
+url = "http://www.showmax.com.tr/canliyayini"
 
 # Optional headers to avoid blocking
 headers = {
